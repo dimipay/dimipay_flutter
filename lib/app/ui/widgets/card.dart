66b62e52c8f84b41ad2c8f7788ff9db9f38b56b2
uglyonlytoday <superjang3357@naver.com>
2021-12-20 12:53:16 +0900

    Card 위젯 isHighlighted 속성 추가

--- lib/app/ui/widgets/card.dart
+++ lib/app/ui/widgets/card.dart
@@ -5,27 +5,30 @@ class DPCard extends StatelessWidget {
   final Widget child;
   final EdgeInsets padding;
   final Color backgroundColor;
+  final double? width;
+  final double? height;
+  final bool isHighlighted;
   const DPCard({
     Key? key,
     required this.child,
     this.borderRadius = 12,
     this.padding = EdgeInsets.zero,
     this.backgroundColor = Colors.white,
+    this.width,
+    this.height,
+    this.isHighlighted = false,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: width,
+      height: height,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(borderRadius),
-        boxShadow: const [
-          BoxShadow(color: Color.fromRGBO(0, 0, 0, 0.06), offset: Offset(0, 2), blurRadius: 12),
-        ],
+        boxShadow: const [BoxShadow(color: Color.fromRGBO(0, 0, 0, 0.06), offset: Offset(0, 2), blurRadius: 12)],
         color: backgroundColor,
-        border: Border.all(
-          color: const Color.fromRGBO(215, 215, 215, 1),
-          width: 1,
-        ),
+        border: isHighlighted ? Border.all(color: const Color.fromRGBO(46, 164, 171, 1), width: 2) : Border.all(color: const Color.fromRGBO(215, 215, 215, 1), width: 1),
       ),
       padding: padding,
       child: child,
