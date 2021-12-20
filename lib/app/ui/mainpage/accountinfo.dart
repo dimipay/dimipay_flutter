b9ec80220ccf1535b627f8acd232dd08793640e0
uglyonlytoday <superjang3357@naver.com>
2021-12-20 20:58:07 +0900

    필요 없는 import 정리

--- lib/app/ui/mainpage/accountinfo.dart
+++ lib/app/ui/mainpage/accountinfo.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class AccountInfoPage extends StatelessWidget {
   const AccountInfoPage({Key? key}) : super(key: key);
