12e304b0507d49e5d7551ed637dc4b0cfa8f525d
uglyonlytoday <superjang3357@naver.com>
2021-12-21 16:16:39 +0900

    EditCardNamePage 구현 arrow_right_small.svg 추가

--- lib/app/ui/edit_card_name.dart/edit_card_name.dart
+++ lib/app/ui/edit_card_name.dart/edit_card_name.dart
@@ -1,3 +1,4 @@
+import 'package:dimipay/app/ui/widgets/button.dart';
 import 'package:dimipay/app/ui/widgets/payment.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
@@ -8,22 +9,70 @@ class EditCardNamePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Edit Card Name Page')),
-      body: DPPaymentCard(
-        color: const Color(0xFF766C62),
+      appBar: AppBar(),
+      body: SafeArea(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: const [
-                Text('개돼지', style: TextStyle(fontFamily: 'NEXON Lv1 Gothic', fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white)),
-                SizedBox(height: 4),
-                Text('2158', style: TextStyle(fontFamily: 'NEXON Lv1 Gothic', fontSize: 16, color: Color.fromRGBO(255, 255, 255, 0.6))),
-              ],
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text(
+                    '카드의 이름을 입력해주세요',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+                  ),
+                  const SizedBox(height: 12),
+                  DPPaymentCard(
+                    color: const Color(0xFF766C62),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: const [
+                            TextField(
+                              cursorColor: Colors.white,
+                              style: TextStyle(fontFamily: 'NEXON Lv1 Gothic', fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
+                              decoration: InputDecoration(
+                                isDense: true,
+                                contentPadding: EdgeInsets.all(0),
+                                border: InputBorder.none,
+                                hintText: '카드 이름',
+                                hintStyle: TextStyle(fontFamily: 'NEXON Lv1 Gothic', fontSize: 18, fontWeight: FontWeight.bold, color: Color.fromRGBO(255, 255, 255, 0.4)),
+                              ),
+                            ),
+                            SizedBox(height: 4),
+                            Text('2158', style: TextStyle(fontFamily: 'NEXON Lv1 Gothic', fontSize: 16, color: Color.fromRGBO(255, 255, 255, 0.6))),
+                          ],
+                        ),
+                        SvgPicture.asset('asset/images/kb_logo.svg'),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 12),
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const Text(
+                        '다음에 설정에서 바꿀게요',
+                        style: TextStyle(color: Color.fromRGBO(0, 0, 0, 0.4)),
+                      ),
+                      const SizedBox(width: 6),
+                      SvgPicture.asset('asset/images/arrow_right_small.svg'),
+                    ],
+                  )
+                ],
+              ),
             ),
-            SvgPicture.asset('asset/images/kb_logo.svg'),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24),
+              child: DPMediumTextButton(text: '다음', width: double.infinity),
+            ),
+            const SizedBox(height: 24),
           ],
         ),
       ),
