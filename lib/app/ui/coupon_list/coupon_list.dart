3cd6211e36416aec97cf3a5fb51a51948d15400b
uglyonlytoday <superjang3357@naver.com>
2021-12-22 21:06:07 +0900

    CouponListPage 구현

--- lib/app/ui/coupon_list/coupon_list.dart
+++ lib/app/ui/coupon_list/coupon_list.dart
@@ -1,5 +1,7 @@
+import 'package:dimipay/app/ui/ui_asset.dart';
 import 'package:dimipay/app/ui/widgets/card.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class CouponListPage extends StatelessWidget {
   const CouponListPage({Key? key}) : super(key: key);
@@ -8,7 +10,79 @@ class CouponListPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('쿠폰')),
-      body: Container(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+            children: [
+              const SizedBox(height: 24),
+              DPCouponCard(title: '공업수학', subtitle: '이병혁 선생님 발행', price: 500, expireAt: DateTime(2021, 10, 12)),
+              const SizedBox(height: 16),
+              DPCouponCard(title: '이병혁 선생님의 쿠폰', subtitle: '이병혁 선생님 발행', price: 500, expireAt: DateTime(2021, 10, 12)),
+              const SizedBox(height: 24),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Text(
+                    '만료된 쿠폰 보기',
+                    style: TextStyle(color: Color.fromRGBO(0, 0, 0, 0.4)),
+                  ),
+                  const SizedBox(width: 12),
+                  SvgPicture.asset('asset/images/arrow_down.svg', color: const Color.fromRGBO(0, 0, 0, 0.4)),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class DPCouponCard extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final int price;
+  final DateTime expireAt;
+  const DPCouponCard({Key? key, required this.title, required this.subtitle, required this.price, required this.expireAt}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DPCard(
+      padding: const EdgeInsets.all(24),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
+                style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+              ),
+              const SizedBox(height: 6),
+              Text(
+                subtitle,
+                style: const TextStyle(color: Color.fromRGBO(0, 0, 0, 0.4)),
+              ),
+            ],
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text(
+                '$price원',
+                style: const TextStyle(color: mainColor, fontWeight: FontWeight.w600, fontSize: 16),
+              ),
+              const SizedBox(height: 6),
+              Text(
+                '~ ${expireAt.year}년 ${expireAt.month}월 ${expireAt.day}일',
+                style: const TextStyle(color: Color.fromRGBO(0, 0, 0, 0.4)),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
