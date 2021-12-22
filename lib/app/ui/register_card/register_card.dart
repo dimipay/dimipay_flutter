a30f434ed53e8def5441dbf1fe9219a2b6de3364
uglyonlytoday <superjang3357@naver.com>
2021-12-22 16:27:30 +0900

    RegisterCardPage 키보드가 올라갔을 때 레이아웃이 깨지던 이슈 수정

--- lib/app/ui/register_card/register_card.dart
+++ lib/app/ui/register_card/register_card.dart
@@ -14,42 +14,44 @@ class RegisterCardPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 24),
-                child: Column(
-                  children: [
-                    const SizedBox(height: 24),
-                    Container(height: 322, color: Colors.black),
-                    const SizedBox(height: 32),
-                    const DPTextField(
-                      label: '카드 번호',
-                      hintText: '카드 번호 16자리를 입력해주세요',
-                    ),
-                    const SizedBox(height: 18),
-                    Row(
-                      children: const [
-                        Flexible(
-                          child: DPTextField(
-                            label: '유효기간',
-                            hintText: 'MM/YY',
+            Flexible(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24),
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 24),
+                      Container(height: 322, color: Colors.black),
+                      const SizedBox(height: 32),
+                      const DPTextField(
+                        label: '카드 번호',
+                        hintText: '카드 번호 16자리를 입력해주세요',
+                      ),
+                      const SizedBox(height: 18),
+                      Row(
+                        children: const [
+                          Flexible(
+                            child: DPTextField(
+                              label: '유효기간',
+                              hintText: 'MM/YY',
+                            ),
                           ),
-                        ),
-                        SizedBox(width: 18),
-                        Flexible(
-                          child: DPTextField(
-                            label: '생년 월일',
-                            hintText: '4자리로 입력',
+                          SizedBox(width: 18),
+                          Flexible(
+                            child: DPTextField(
+                              label: '생년 월일',
+                              hintText: '4자리로 입력',
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 18),
-                    const DPTextField(
-                      label: '비밀번호',
-                      hintText: '앞 2자리를 입력해주세요',
-                    ),
-                  ],
+                        ],
+                      ),
+                      const SizedBox(height: 18),
+                      const DPTextField(
+                        label: '비밀번호',
+                        hintText: '앞 2자리를 입력해주세요',
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
