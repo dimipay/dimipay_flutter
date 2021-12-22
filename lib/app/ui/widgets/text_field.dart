ea57e01241763b165eb3b4d8d85c5f731fe05cf1
uglyonlytoday <superjang3357@naver.com>
2021-12-22 12:57:17 +0900

    DPTextField 위젯 const 적용

--- lib/app/ui/widgets/text_field.dart
+++ lib/app/ui/widgets/text_field.dart
@@ -10,16 +10,16 @@ class DPTextField extends StatelessWidget {
     return ClipRRect(
       borderRadius: BorderRadius.circular(8),
       child: TextField(
-        style: TextStyle(fontSize: 18),
+        style: const TextStyle(fontSize: 18),
         cursorColor: Colors.black,
         decoration: InputDecoration(
           hintText: hintText,
           labelText: label,
           border: InputBorder.none,
-          fillColor: Color(0xFFF6F6F6),
+          fillColor: const Color(0xFFF6F6F6),
           filled: true,
-          hintStyle: TextStyle(fontSize: 18, color: Color.fromRGBO(0, 0, 0, 0.4)),
-          floatingLabelStyle: TextStyle(color: Color.fromRGBO(0, 0, 0, 0.6), fontWeight: FontWeight.w500, fontSize: 12),
+          hintStyle: const TextStyle(fontSize: 18, color: Color.fromRGBO(0, 0, 0, 0.4)),
+          floatingLabelStyle: const TextStyle(color: Color.fromRGBO(0, 0, 0, 0.6), fontWeight: FontWeight.w500, fontSize: 12),
         ),
       ),
     );
