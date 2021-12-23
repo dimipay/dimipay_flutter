98db77d3e26787319e756ad597f8203822c509ad
uglyonlytoday <superjang3357@naver.com>
2021-12-23 20:29:00 +0900

    ChangePasswordPage 레이아웃 수정

--- lib/app/ui/login/change_password.dart
+++ lib/app/ui/login/change_password.dart
@@ -11,14 +11,10 @@ class ChangePasswordPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          "디미고 계정 비밀번호 변경",
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
-        ),
+        title: const Text("디미고 계정 비밀번호 변경"),
       ),
       body: SafeArea(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
             Flexible(
@@ -26,20 +22,18 @@ class ChangePasswordPage extends StatelessWidget {
                 child: Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 24),
                   child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: const [
                       SizedBox(height: 24),
                       Text(
                         '보안을 위해 비밀번호를 변경해주세요',
-                        style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
                       ),
-                      SizedBox(height: 18),
+                      SizedBox(height: 16),
                       Text(
                         '비밀번호가 초기 설정 이후 변경되지 않았거나 아이디와 똑같이 설정되어 있어요.',
-                        style: TextStyle(fontSize: 16),
                       ),
-                      SizedBox(height: 18),
+                      SizedBox(height: 16),
                       CheckPassword(title: "숫자가 포함되어있어요", checked: true),
                       SizedBox(height: 12),
                       CheckPassword(title: "7자 이상이예요", checked: false),
@@ -47,20 +41,11 @@ class ChangePasswordPage extends StatelessWidget {
                       CheckPassword(title: "아이디와 동일하지 않아요", checked: false),
                       SizedBox(height: 12),
                       SizedBox(height: 24),
-                      DPTextField(
-                        isPassword: true,
-                        label: "현재 비밀번호",
-                      ),
+                      DPTextField(label: "현재 비밀번호", isPassword: true),
                       SizedBox(height: 16),
-                      DPTextField(
-                        isPassword: true,
-                        label: "변경할 비밀번호",
-                      ),
+                      DPTextField(label: "변경할 비밀번호", isPassword: true),
                       SizedBox(height: 16),
-                      DPTextField(
-                        isPassword: true,
-                        label: "한번 더 입력해주세요",
-                      ),
+                      DPTextField(label: "한번 더 입력해주세요", isPassword: true),
                     ],
                   ),
                 ),
@@ -95,34 +80,15 @@ class CheckPassword extends StatefulWidget {
 class _CheckPasswordState extends State<CheckPassword> {
   @override
   Widget build(BuildContext context) {
-    if (widget.checked) {
-      return Row(
-        children: [
-          SvgPicture.asset("asset/images/check.svg"),
-          const SizedBox(
-            width: 4,
-          ),
-          Text(
-            widget.title,
-            style: const TextStyle(color: mainColor, decoration: TextDecoration.underline),
-          )
-        ],
-      );
-    } else {
-      return Row(
-        children: [
-          SvgPicture.asset("asset/images/not_check.svg"),
-          const SizedBox(
-            width: 4,
-          ),
-          Text(
-            widget.title,
-            style: const TextStyle(
-              color: Color.fromRGBO(0, 0, 0, 0.4),
-            ),
-          )
-        ],
-      );
-    }
+    return Row(
+      children: [
+        widget.checked ? SvgPicture.asset("asset/images/check.svg") : SvgPicture.asset("asset/images/not_check.svg", color: const Color.fromRGBO(0, 0, 0, 0.4)),
+        const SizedBox(width: 4),
+        Text(
+          widget.title,
+          style: widget.checked ? const TextStyle(color: mainColor, decoration: TextDecoration.underline, fontWeight: FontWeight.w600) : const TextStyle(color: Color.fromRGBO(0, 0, 0, 0.4)),
+        )
+      ],
+    );
   }
 }
