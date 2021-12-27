4acb304fcd4490cb8647b9d939b843889eb8b884
uglyonlytoday <superjang3357@naver.com>
2021-12-27 21:07:20 +0900

    Design System Color 정의

--- lib/app/ui/ui_asset.dart
+++ lib/app/ui/ui_asset.dart
@@ -1,3 +1,14 @@
 import 'package:flutter/material.dart';
 
-const mainColor = Color.fromRGBO(46, 164, 171, 1);
+class DPColors {
+  DPColors._();
+
+  static const Color primary = Color(0xFF000000);
+  static const Color darkGray = Color(0xFF404040);
+  static const Color gray = Color(0xFFC0C0C0);
+  static const Color lightGray = Color(0xFFF2F2F2);
+
+  static const Color primaryText = Color(0xFF000000);
+  static const Color contentText = Color(0xFF212121);
+  static const Color secondaryText = Color(0xFF8A8A8E);
+}
