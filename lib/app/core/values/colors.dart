694eb48aff2112cfefc33f7875e76cc394e40f26
uglyonlytoday <superjang3357@naver.com>
2022-02-11 22:31:27 +0900

    Color data update

--- lib/app/core/values/colors.dart
+++ lib/app/core/values/colors.dart
@@ -1,5 +1,13 @@
+// ignore_for_file: constant_identifier_names
+
 import 'package:flutter/material.dart';
 
 abstract class DPColors {
   static const MAIN_THEME = Color(0xFF2EA4AB);
+  static const DARK1 = Color(0xFF2C3939);
+  static const DARK2 = Color(0xFF556161);
+  static const DARK3 = Color(0xFF808888);
+  static const DARK4 = Color(0xFFABB0B0);
+  static const DARK5 = Color(0xFFD5D7D7);
+  static const DARK6 = Color(0xFFF4F5F5);
 }
