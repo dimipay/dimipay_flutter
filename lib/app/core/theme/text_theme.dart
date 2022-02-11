9203342a95212066bf96481d37288c2efdc590e2
uglyonlytoday <superjang3357@naver.com>
2022-02-11 23:24:34 +0900

    Add text theme data

---
+++ lib/app/core/theme/text_theme.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:dimipay/app/core/theme/color_theme.dart';
+import 'package:flutter/material.dart';
+
+abstract class DPTextTheme {
+  static const DECORATIVE = TextStyle(fontFamily: 'NEXON Lv1 Gothic', fontWeight: FontWeight.bold, fontSize: 20, height: 1.2, color: DPColors.DARK1);
+  static const PAGE_HEADER = TextStyle(fontFamily: 'Pretendard', fontWeight: FontWeight.bold, fontSize: 20, height: 1.2, color: DPColors.DARK1);
+  static const PAGE_DESCRIPTION = TextStyle(fontFamily: 'Pretendard', fontSize: 16, height: 1.2, color: Color(0xFFABB0B0));
+  static const SECTION_HEADER = TextStyle(fontFamily: 'Pretendard', fontWeight: FontWeight.bold, fontSize: 18, height: 1.2, color: DPColors.DARK1);
+  static const REGULAR = TextStyle(fontFamily: 'Pretendard', fontSize: 16, height: 1.2, color: DPColors.DARK1);
+  static const READABLE = TextStyle(fontFamily: 'Pretendard', fontWeight: FontWeight.w500, fontSize: 20, height: 1.2, color: DPColors.DARK1);
+  static const REGULAR_IMPORTANT = TextStyle(fontFamily: 'Pretendard', fontWeight: FontWeight.w600, fontSize: 16, height: 1.2, color: DPColors.DARK1);
+  static const DESCRIPTION = TextStyle(fontFamily: 'Pretendard', fontSize: 16, height: 1.2, color: Color(0xFFABB0B0));
+  static const DESCRIPTION_IMPORTANT = TextStyle(fontFamily: 'Pretendard', fontSize: 16, height: 1.2, color: DPColors.DARK1);
+}
