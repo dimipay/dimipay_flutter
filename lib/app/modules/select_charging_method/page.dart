39a856aeb705b0b92d122793cbad940a5f7c1b11
uglyonlytoday <superjang3357@naver.com>
2022-02-12 21:57:29 +0900

    SelectChargingMethodPage redesign 구현

--- lib/app/modules/select_charging_method/page.dart
+++ lib/app/modules/select_charging_method/page.dart
@@ -1,14 +1,85 @@
+import 'package:dimipay/app/core/theme/color_theme.dart';
+import 'package:dimipay/app/core/theme/text_theme.dart';
 import 'package:dimipay/app/widgets/card.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
-class SelectChargingMethodPage extends StatefulWidget {
+class SelectChargingMethodPage extends StatelessWidget {
   const SelectChargingMethodPage({Key? key}) : super(key: key);
 
-  @override
-  _SelectChargingMethodPageState createState() => _SelectChargingMethodPageState();
-}
+  Widget _simplePayMethodCard(String text, String imagePath) {
+    return Expanded(
+      child: AspectRatio(
+        aspectRatio: 1,
+        child: Container(
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(12), color: DPColors.DARK6),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(imagePath),
+              const SizedBox(height: 12),
+              Text(text, style: DPTextTheme.REGULAR),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 
-class _SelectChargingMethodPageState extends State<SelectChargingMethodPage> {
+  Widget _simplaPayArea() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text('간편 결제', style: DPTextTheme.DESCRIPTION),
+        const SizedBox(height: 12),
+        Row(
+          children: [
+            _simplePayMethodCard('카카오페이', 'asset/images/payment_kakao.png'),
+            const SizedBox(width: 12),
+            _simplePayMethodCard('토스결제', 'asset/images/payment_toss.png'),
+            const SizedBox(width: 12),
+            _simplePayMethodCard('네이버페이', 'asset/images/payment_toss.png'),
+          ],
+        )
+      ],
+    );
+  }
+
+  Widget _standardPayMethodItem(String title, String description) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 12),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Flexible(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(title, style: DPTextTheme.REGULAR),
+                const SizedBox(height: 4),
+                Text(description, style: DPTextTheme.DESCRIPTION),
+              ],
+            ),
+          ),
+          SizedBox(width: 64),
+          SvgPicture.asset('asset/images/arrow_right_6.svg'),
+        ],
+      ),
+    );
+  }
+
+  Widget _standardPayArea() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text('일반결제', style: DPTextTheme.DESCRIPTION),
+        const SizedBox(height: 12),
+        _standardPayMethodItem('통장 등록', '결제할 때 마다 통장에서 돈이 빠져나가요'),
+        _standardPayMethodItem('입금', '디미페이의 계좌로 송금해서 포인트 잔액을 충전해요'),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,83 +88,23 @@ class _SelectChargingMethodPageState extends State<SelectChargingMethodPage> {
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 24),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    "어떻게 충전할까요?",
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
-                  ),
-                  const SizedBox(
-                    height: 48,
-                  ),
-                  const Text(
-                    "간편결제",
-                    style: TextStyle(fontSize: 16, color: Color.fromRGBO(0, 0, 0, 0.4)),
-                  ),
-                  const SizedBox(
-                    height: 12,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              Flexible(
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.vertical,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      DPSimplePaymentMethodCard(
-                        text: '카카오페이',
-                        image: Image.asset('asset/images/payment_kakao.png'),
-                      ),
-                      const SizedBox(width: 12),
-                      DPSimplePaymentMethodCard(
-                        text: '토스결제',
-                        image: Image.asset('asset/images/payment_toss.png'),
-                      ),
-                      const SizedBox(width: 12),
-                      DPSimplePaymentMethodCard(
-                        text: '네이버페이',
-                        image: Image.asset('asset/images/payment_toss.png'),
-                      ),
+                      const Text('어떻게 충전할까요?', style: DPTextTheme.SECTION_HEADER),
+                      const SizedBox(height: 36),
+                      _simplaPayArea(),
+                      const SizedBox(height: 36),
+                      _standardPayArea(),
                     ],
                   ),
-                  const SizedBox(
-                    height: 24,
-                  ),
-                  const Text("일반결제", style: TextStyle(color: Color.fromRGBO(0, 0, 0, 0.5))),
-                  const SizedBox(
-                    height: 12,
-                  ),
-                  DPCard(
-                    width: double.infinity,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
-                        Text('통장 등록', style: TextStyle(fontSize: 16)),
-                        SizedBox(height: 4),
-                        Text(
-                          '결제할 때 마다 통장에서 돈이 빠져나가요',
-                          style: TextStyle(color: Color.fromRGBO(0, 0, 0, 0.4)),
-                        ),
-                      ],
-                    ),
-                    padding: const EdgeInsets.all(24),
-                  ),
-                  const SizedBox(height: 12),
-                  DPCard(
-                    width: double.infinity,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
-                        Text('입금', style: TextStyle(fontSize: 16)),
-                        SizedBox(height: 4),
-                        Text(
-                          '디미페이의 통장으로 송금해서 포인트 잔액을 충전해요',
-                          style: TextStyle(color: Color.fromRGBO(0, 0, 0, 0.4)),
-                        ),
-                      ],
-                    ),
-                    padding: const EdgeInsets.all(24),
-                  ),
-                ],
+                ),
               ),
               Padding(
                 padding: const EdgeInsets.only(bottom: 24),
@@ -123,39 +134,6 @@ class _SelectChargingMethodPageState extends State<SelectChargingMethodPage> {
                   padding: const EdgeInsets.all(24),
                 ),
               ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class DPSimplePaymentMethodCard extends StatelessWidget {
-  final Image image;
-  final String text;
-
-  const DPSimplePaymentMethodCard({
-    Key? key,
-    required this.text,
-    required this.image,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: DPCard(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 24),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              image,
-              const SizedBox(height: 12),
-              Text(
-                text,
-                style: const TextStyle(fontSize: 16),
-              )
             ],
           ),
         ),
