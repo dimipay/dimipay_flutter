c57e8eee83fb687c36d3779367b6f6a6239f8179
uglyonlytoday <superjang3357@naver.com>
2022-02-12 22:45:11 +0900

    DPPaymentCard 수정

--- lib/app/widgets/payment.dart
+++ lib/app/widgets/payment.dart
@@ -5,23 +5,25 @@ class DPPaymentCard extends StatelessWidget {
   final Color color;
   final String cardName;
   final String cardNumber;
-  const DPPaymentCard({Key? key, required this.color, required this.cardName, required this.cardNumber}) : super(key: key);
+  final double? width;
+  const DPPaymentCard({Key? key, required this.color, required this.cardName, required this.cardNumber, this.width}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: double.infinity,
-      padding: const EdgeInsets.only(left: 20, right: 20, top: 30, bottom: 20),
+      width: width ?? double.infinity,
+      padding: const EdgeInsets.all(18),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(12),
         color: color,
         boxShadow: const [BoxShadow(color: Color.fromRGBO(0, 0, 0, 0.12), offset: Offset(0, 4), blurRadius: 72)],
       ),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(cardNumber, style: DPTextTheme.DESCRIPTION),
+          Text(cardNumber, style: const TextStyle(fontSize: 16, color: Color.fromRGBO(255, 255, 255, 0.5))),
           const SizedBox(height: 4),
-          Text(cardName, style: DPTextTheme.SECTION_HEADER),
+          Text(cardName, style: const TextStyle(fontSize: 18, color: Colors.white, fontWeight: FontWeight.bold)),
         ],
       ),
     );
