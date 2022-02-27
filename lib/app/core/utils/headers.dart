a93b7820de03ff753c150f1885bb40fcddeb2cb5
Minyong Oh <dhalsdyd@gmail.com>
2022-02-27 20:21:05 +0900

    changes after review

--- lib/app/core/utils/headers.dart
+++ lib/app/core/utils/headers.dart
@@ -1,10 +1,19 @@
+import 'package:dimipay/app/data/services/auth/service.dart';
+import 'package:get/get.dart';
+
 class HeadersAPI {
-  Map<String, String> getHeaders() {
+  Map<String, String>? getHeaders({bool auth = false}) {
+    if (auth) {
+      return _getAuthorization();
+    }
     return {"Content-Type": "application/json"};
   }
 
-  Map<String, String> getAuthorization() {
-    String token = "";
+  Map<String, String> _getAuthorization() {
+    AuthService service = Get.find<AuthService>();
+
+    String token = service.token;
+
     return {"Authorization": "Bearer $token"};
   }
 }
