1ec084f349ee02911066694eceec2c96b300229d
uglyonlytoday <superjang3357@naver.com>
2022-03-01 14:50:48 +0900

    User model 수정

--- lib/app/data/models/user.g.dart
+++ lib/app/data/models/user.g.dart
@@ -7,29 +7,25 @@ part of 'user.dart';
 // **************************************************************************
 
 User _$UserFromJson(Map<String, dynamic> json) => User(
-      systemId: json['systemId'] as int?,
-      name: json['name'] as String?,
-      password: json['password'] as String?,
-    )
-      ..studentNumber = json['studentNumber'] as String?
-      ..accountName = json['accountName'] as String?
-      ..profileImage = json['profileImage'] as String?
-      ..changedPassword = json['changedPassword'] as String?
-      ..payMoney = (json['payMoney'] as num?)?.toDouble()
-      ..receivedCoupons = json['receivedCoupons'] as List<dynamic>?
-      ..paymentMethods = json['paymentMethods'] as List<dynamic>?
-      ..token = json['token'] as String?;
+      systemId: json['systemId'] as String,
+      createdAt: DateTime.parse(json['createdAt'] as String),
+      updatedAt: DateTime.parse(json['updatedAt'] as String),
+      isDisabled: json['isDisabled'] as bool,
+      accountName: json['accountName'] as String,
+      name: json['name'] as String,
+      isTeacher: json['isTeacher'] as bool?,
+      profileImage: json['profileImage'] as String?,
+      studentNumber: json['studentNumber'] as String?,
+    );
 
 Map<String, dynamic> _$UserToJson(User instance) => <String, dynamic>{
       'systemId': instance.systemId,
-      'name': instance.name,
-      'studentNumber': instance.studentNumber,
+      'createdAt': instance.createdAt.toIso8601String(),
+      'updatedAt': instance.updatedAt.toIso8601String(),
+      'isDisabled': instance.isDisabled,
       'accountName': instance.accountName,
+      'name': instance.name,
+      'isTeacher': instance.isTeacher,
       'profileImage': instance.profileImage,
-      'password': instance.password,
-      'changedPassword': instance.changedPassword,
-      'payMoney': instance.payMoney,
-      'receivedCoupons': instance.receivedCoupons,
-      'paymentMethods': instance.paymentMethods,
-      'token': instance.token,
+      'studentNumber': instance.studentNumber,
     };
