4699693ebfbdb435d5c0f9e6c3fc38bb6feb722c
uglyonlytoday <superjang3357@naver.com>
2022-03-08 20:04:26 +0900

    UserController CouponController binding 위치 변경

--- lib/app/modules/account_info/binding.dart
+++ lib/app/modules/account_info/binding.dart
@@ -1,11 +1,6 @@
-import 'package:dimipay/app/data/provider/api.dart';
-import 'package:dimipay/app/modules/account_info/controller.dart';
-import 'package:dimipay/app/modules/account_info/repository.dart';
 import 'package:get/get.dart';
 
 class AccountInfoPageBinding implements Bindings {
   @override
-  void dependencies() {
-    Get.lazyPut<UserController>(() => UserController(UserRepository(ApiProvider())));
-  }
+  void dependencies() {}
 }
