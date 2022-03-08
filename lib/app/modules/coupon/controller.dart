c5636c2b76cb85d38ea01cba965abcc7da7945da
uglyonlytoday <superjang3357@naver.com>
2022-03-08 18:19:20 +0900

    CouponPage pull-to-refresh 구현

--- lib/app/modules/coupon/controller.dart
+++ lib/app/modules/coupon/controller.dart
@@ -1,5 +1,5 @@
 import 'package:dimipay/app/data/models/coupon.dart';
-import 'package:dimipay/app/modules/coupon_list/repository.dart';
+import 'package:dimipay/app/modules/coupon/repository.dart';
 import 'package:get/get.dart';
 
 class CouponController extends GetxController with StateMixin<List<Coupon>> {
@@ -17,5 +17,9 @@ class CouponController extends GetxController with StateMixin<List<Coupon>> {
   Future getCoupons() async {
     coupons.value = await repository.get();
     change(coupons.value, status: RxStatus.success());
+  }
+
+  Future<void> refreshCoupons() async {
+    await getCoupons();
   }
 }
--- lib/app/modules/coupon/page.dart
+++ lib/app/modules/coupon/page.dart
@@ -1,8 +1,8 @@
 import 'package:dimipay/app/core/theme/color_theme.dart';
 import 'package:dimipay/app/core/theme/text_theme.dart';
 import 'package:dimipay/app/data/models/coupon.dart';
-import 'package:dimipay/app/modules/coupon_list/controller.dart';
-import 'package:dimipay/app/modules/coupon_list/widget.dart/coupon_list_item.dart';
+import 'package:dimipay/app/modules/coupon/controller.dart';
+import 'package:dimipay/app/modules/coupon/widget.dart/coupon_list_item.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
@@ -36,51 +36,56 @@ class CouponPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        bottom: false,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 32),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
-                  SizedBox(height: 36),
-                  Text('쿠폰', style: DPTextTheme.PAGE_HEADER),
-                  SizedBox(height: 32),
-                ],
-              ),
-              Flexible(
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      Row(
-                        children: [
-                          SvgPicture.asset('asset/images/circled_question.svg'),
-                          const SizedBox(width: 12),
-                          const Text('쿠폰은 어떻게 사용하나요?', style: DPTextTheme.DESCRIPTION),
-                        ],
-                      ),
-                      const SizedBox(height: 36),
-                      _couponArea(),
-                      // Row(
-                      //   mainAxisSize: MainAxisSize.min,
-                      //   children: [
-                      //     const Text(
-                      //       '만료된 쿠폰 보기',
-                      //       style: DPTextTheme.DESCRIPTION,
-                      //     ),
-                      //     const SizedBox(width: 12),
-                      //     SvgPicture.asset('asset/images/arrow_down.svg', color: DPColors.DARK4),
-                      //   ],
-                      // ),
-                      // const SizedBox(height: 36),
-                    ],
+      body: RefreshIndicator(
+        onRefresh: couponController.refreshCoupons,
+        color: DPColors.MAIN_THEME,
+        child: SafeArea(
+          bottom: false,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 32),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 36),
+                    const Text('쿠폰', style: DPTextTheme.PAGE_HEADER),
+                    const SizedBox(height: 32),
+                    Row(
+                      children: [
+                        SvgPicture.asset('asset/images/circled_question.svg'),
+                        const SizedBox(width: 12),
+                        const Text('쿠폰은 어떻게 사용하나요?', style: DPTextTheme.DESCRIPTION),
+                      ],
+                    ),
+                    const SizedBox(height: 36),
+                  ],
+                ),
+                Expanded(
+                  child: SingleChildScrollView(
+                    physics: const BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
+                    child: Column(
+                      children: [
+                        _couponArea(),
+                        // Row(
+                        //   mainAxisSize: MainAxisSize.min,
+                        //   children: [
+                        //     const Text(
+                        //       '만료된 쿠폰 보기',
+                        //       style: DPTextTheme.DESCRIPTION,
+                        //     ),
+                        //     const SizedBox(width: 12),
+                        //     SvgPicture.asset('asset/images/arrow_down.svg', color: DPColors.DARK4),
+                        //   ],
+                        // ),
+                        // const SizedBox(height: 36),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
