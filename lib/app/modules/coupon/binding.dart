412913f1a803b9d92ef41395f6e7818ab1cb9813
uglyonlytoday <superjang3357@naver.com>
2022-03-08 18:19:20 +0900

    CouponPage pull-to-refresh 구현

--- lib/app/modules/coupon/binding.dart
+++ lib/app/modules/coupon/binding.dart
@@ -1,6 +1,6 @@
 import 'package:dimipay/app/data/provider/api.dart';
-import 'package:dimipay/app/modules/coupon_list/controller.dart';
-import 'package:dimipay/app/modules/coupon_list/repository.dart';
+import 'package:dimipay/app/modules/coupon/controller.dart';
+import 'package:dimipay/app/modules/coupon/repository.dart';
 import 'package:get/get.dart';
 
 class CouponPageBinding implements Bindings {
