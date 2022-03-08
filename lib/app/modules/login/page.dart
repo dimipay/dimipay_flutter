b0029d7dbee7123a2808fe4625f9038c2d5bb287
Minyong Oh <dhalsdyd@gmail.com>
2022-03-08 18:27:53 +0900

    보다 더 나은 로딩

--- lib/app/modules/login/page.dart
+++ lib/app/modules/login/page.dart
@@ -38,11 +38,13 @@ class LoginPage extends GetView<LoginPageController> {
               ),
             ),
             const SizedBox(height: 36),
-            DPKeyboardReactiveButton(
-              onTap: controller.login,
-              padding: const EdgeInsets.only(left: 24, right: 24, bottom: 24),
-              child: const Text('다음', style: TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.w600)),
-            ),
+            controller.obx(
+                (state) => DPKeyboardReactiveButton(
+                      onTap: controller.login,
+                      padding: const EdgeInsets.only(left: 24, right: 24, bottom: 24),
+                      child: const Text('다음', style: TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.w600)),
+                    ),
+                onLoading: CircularProgressIndicator()),
           ],
         ),
       ),
