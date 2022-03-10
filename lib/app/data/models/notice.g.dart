2068484b64db947495cd4e0e720e603b865305c0
uglyonlytoday <superjang3357@naver.com>
2022-03-10 22:30:47 +0900

    Notice model 수정

--- lib/app/data/models/notice.g.dart
+++ lib/app/data/models/notice.g.dart
@@ -7,27 +7,13 @@ part of 'notice.dart';
 // **************************************************************************
 
 Notice _$NoticeFromJson(Map<String, dynamic> json) => Notice(
-      id: json['id'] as String,
       createdAt: DateTime.parse(json['createdAt'] as String),
-      updatedAt: DateTime.parse(json['updatedAt'] as String),
-      startsAt: DateTime.parse(json['startsAt'] as String),
-      endsAt: json['endsAt'] == null
-          ? null
-          : DateTime.parse(json['endsAt'] as String),
       title: json['title'] as String,
       description: json['description'] as String,
-      url: json['url'] as String,
-      authorId: json['authorId'] as String,
     );
 
 Map<String, dynamic> _$NoticeToJson(Notice instance) => <String, dynamic>{
-      'id': instance.id,
       'createdAt': instance.createdAt.toIso8601String(),
-      'updatedAt': instance.updatedAt.toIso8601String(),
-      'startsAt': instance.startsAt.toIso8601String(),
-      'endsAt': instance.endsAt?.toIso8601String(),
       'title': instance.title,
       'description': instance.description,
-      'url': instance.url,
-      'authorId': instance.authorId,
     };
