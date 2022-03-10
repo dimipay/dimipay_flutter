2449e3ae226323a33870f3b660c7c783e6592f1b
uglyonlytoday <superjang3357@naver.com>
2022-03-10 22:30:40 +0900

    Notice model 수정

--- lib/app/data/models/notice.dart
+++ lib/app/data/models/notice.dart
@@ -3,27 +3,15 @@ part 'notice.g.dart';
 
 @JsonSerializable()
 class Notice {
-  String id;
   DateTime createdAt;
-  DateTime updatedAt;
-  DateTime startsAt;
-  DateTime? endsAt;
 
   String title;
   String description;
-  String url;
-  String authorId;
 
   Notice({
-    required this.id,
     required this.createdAt,
-    required this.updatedAt,
-    required this.startsAt,
-    this.endsAt,
     required this.title,
     required this.description,
-    required this.url,
-    required this.authorId,
   });
 
   factory Notice.fromJson(Map<String, dynamic> json) => _$NoticeFromJson(json);
