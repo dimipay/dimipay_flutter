d38637adc560d04e21b2b19bc2fc597bea962973
Minyong Oh <dhalsdyd@gmail.com>
2022-03-13 21:21:31 +0900

    ready

---
+++ lib/app/modules/transaction_history/binding.dart
@@ -0,0 +1,11 @@
+import 'package:dimipay/app/data/provider/api.dart';
+import 'package:dimipay/app/modules/transaction_history/controller.dart';
+import 'package:dimipay/app/modules/transaction_history/respository.dart';
+import 'package:get/get.dart';
+
+class TransactionBinding implements Bindings {
+  @override
+  void dependencies() {
+    Get.put<TransactionController>(TransactionController(TransactionRepository(ApiProvider())));
+  }
+}
---
+++ lib/app/modules/transaction_history/controller.dart
@@ -0,0 +1,24 @@
+import 'package:dimipay/app/data/models/transaction.dart';
+import 'package:dimipay/app/modules/transaction_history/respository.dart';
+import 'package:get/get.dart';
+
+class TransactionController extends GetxController with StateMixin {
+  final TransactionRepository repository;
+  TransactionController(this.repository);
+  Rx<List<Transaction>> transaction = Rx([]);
+
+  @override
+  void onInit() {
+    getTransaction();
+    super.onInit();
+  }
+
+  Future getTransaction() async {
+    change(null, status: RxStatus.loading());
+    transaction.value = await repository.get();
+    if (transaction.value != null)
+      change(transaction.value, status: RxStatus.success());
+    else
+      change(null, status: RxStatus.success());
+  }
+}
