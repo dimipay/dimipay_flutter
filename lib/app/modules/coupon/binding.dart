661433c709a5c1ffaa87dbedd2886d7fa821f608
uglyonlytoday <superjang3357@naver.com>
2022-03-14 18:54:34 +0900

    add omitted DI

--- lib/app/modules/coupon/binding.dart
+++ lib/app/modules/coupon/binding.dart
@@ -1,6 +1,11 @@
+import 'package:dimipay/app/data/provider/api.dart';
+import 'package:dimipay/app/modules/coupon/controller.dart';
+import 'package:dimipay/app/modules/coupon/repository.dart';
 import 'package:get/get.dart';
 
 class CouponPageBinding implements Bindings {
   @override
-  void dependencies() {}
+  void dependencies() {
+    Get.lazyPut<CouponController>(() => CouponController(CouponRepository(ApiProvider())));
+  }
 }
