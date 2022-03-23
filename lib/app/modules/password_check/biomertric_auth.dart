b0b5092b73c9833e78f6444f030d81acc5896051
Minyong Oh <dhalsdyd@gmail.com>
2022-03-23 18:36:34 +0900

    Init : biometric_auth page

---
+++ lib/app/modules/password_check/biomertric_auth.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class BiometricAuth extends StatefulWidget {
+  const BiometricAuth({Key? key}) : super(key: key);
+
+  @override
+  State<BiometricAuth> createState() => _BiometricAuthState();
+}
+
+class _BiometricAuthState extends State<BiometricAuth> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
