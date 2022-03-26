0742ed8d1a3f6389563ecd49cebaca554b4d4d65
uglyonlytoday <superjang3357@naver.com>
2022-03-26 14:12:20 +0900

    add NumberPadItem.value

--- lib/app/modules/pin_auth/widget/numberpad_item.dart
+++ lib/app/modules/pin_auth/widget/numberpad_item.dart
@@ -1,18 +1,21 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 class NumberPadItem extends StatelessWidget {
   final Widget child;
-  final void Function()? onTap;
-  const NumberPadItem({Key? key, required this.child, this.onTap}) : super(key: key);
+  final String? value;
+  final void Function(String? value)? onTap;
+  const NumberPadItem({Key? key, required this.child, this.value, this.onTap}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       // color: Color(Random().nextInt(0xffffffff)),
       child: GestureDetector(
-        onTap: onTap,
+        onTap: () {
+          if (onTap != null) {
+            onTap!(value);
+          }
+        },
         child: Center(
           child: child,
         ),
