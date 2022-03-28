187e0b292f4cde03d54d3e57140ec11a8d2e3858
uglyonlytoday <superjang3357@naver.com>
2022-03-28 12:27:48 +0900

    refactor design

--- lib/app/modules/issue_coupon/page.dart
+++ lib/app/modules/issue_coupon/page.dart
@@ -14,51 +14,48 @@ class IssueCouponPage extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 32),
         child: SingleChildScrollView(
           child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            const SizedBox(height: 32),
             const Text(
               "쿠폰은 매점에서 현금과 동일하게 사용할 수 있어요. 발급 내역은 내 정보 페이지의 결제기록에서 볼 수 있어요",
               style: DPTextTheme.REGULAR,
             ),
-            const SizedBox(
-              height: 24,
-            ),
+            const SizedBox(height: 24),
             const DPTextField(
               label: "수령할 학생",
               hintText: "학생의 학번 또는 이름을 입력해주세요",
             ),
-            const SizedBox(
-              height: 18,
-            ),
+            const SizedBox(height: 18),
             const DPTextField(
               label: "금액",
               hintText: "쿠폰의 금액을 입력해주세요",
             ),
-            const SizedBox(
-              height: 18,
-            ),
+            const SizedBox(height: 18),
             const DPTextField(
               label: "사유",
               hintText: "쿠폰 발급 사유를 입력해주세요",
             ),
-            const SizedBox(
-              height: 8,
-            ),
+            const SizedBox(height: 8),
             const Text(
               "발급사유는 학생이 확인할 수 있어요",
               style: DPTextTheme.DESCRIPTION,
             ),
-            const SizedBox(
-              height: 24,
-            ),
-            const Text(
-              "알림 전송",
-              style: DPTextTheme.SECTION_HEADER,
-            ),
+            const SizedBox(height: 24),
             Row(
               children: [
-                const Expanded(
-                  child: Text(
-                    "학생의 스마트폰으로 쿠폰이 발급되었다는 알림이 전송돼요",
-                    style: DPTextTheme.DESCRIPTION,
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text(
+                        "알림 전송",
+                        style: DPTextTheme.REGULAR_IMPORTANT,
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        "학생의 스마트폰으로 쿠폰이 발급되었다는 알림이 전송돼요",
+                        style: DPTextTheme.DESCRIPTION,
+                      ),
+                    ],
                   ),
                 ),
                 const Switch(
