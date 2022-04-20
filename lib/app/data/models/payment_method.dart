3a6f4af1bf55ace99f7886158d221f72d8616cfb
uglyonlytoday <superjang3357@naver.com>
2022-04-20 17:35:59 +0900

    fix Model error

--- lib/app/data/models/payment_method.dart
+++ lib/app/data/models/payment_method.dart
@@ -1,6 +1,8 @@
 import 'package:json_annotation/json_annotation.dart';
 part 'payment_method.g.dart';
 
+// g.dart 파일 생성 : flutter pub run build_runner build --delete-conflicting-outputs
+
 enum PaymentType {
   PREPAID,
   GENERAL,
@@ -10,12 +12,12 @@ enum PaymentType {
 class PaymentMethod {
   String id;
   DateTime createdAt;
-  DateTime updateAt;
+  DateTime updatedAt;
   PaymentType type;
   String? color;
   String? name;
   String ownerId;
-  PaymentMethod({required this.id, required this.createdAt, required this.updateAt, required this.type, required this.color, required this.name, required this.ownerId});
+  PaymentMethod({required this.id, required this.createdAt, required this.updatedAt, required this.type, required this.color, required this.name, required this.ownerId});
 
   factory PaymentMethod.fromJson(Map<String, dynamic> json) => _$PaymentMethodFromJson(json);
   Map<String, dynamic> toJson() => _$PaymentMethodToJson(this);
--- lib/app/data/models/payment_method.g.dart
+++ lib/app/data/models/payment_method.g.dart
@@ -6,20 +6,22 @@ part of 'payment_method.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-PaymentMethod _$PaymentMethodFromJson(Map<String, dynamic> json) => PaymentMethod(
+PaymentMethod _$PaymentMethodFromJson(Map<String, dynamic> json) =>
+    PaymentMethod(
       id: json['id'] as String,
       createdAt: DateTime.parse(json['createdAt'] as String),
-      updateAt: DateTime.parse(json['updateAt'] as String),
+      updatedAt: DateTime.parse(json['updatedAt'] as String),
       type: $enumDecode(_$PaymentTypeEnumMap, json['type']),
       color: json['color'] as String?,
       name: json['name'] as String?,
       ownerId: json['ownerId'] as String,
     );
 
-Map<String, dynamic> _$PaymentMethodToJson(PaymentMethod instance) => <String, dynamic>{
+Map<String, dynamic> _$PaymentMethodToJson(PaymentMethod instance) =>
+    <String, dynamic>{
       'id': instance.id,
       'createdAt': instance.createdAt.toIso8601String(),
-      'updateAt': instance.updateAt.toIso8601String(),
+      'updatedAt': instance.updatedAt.toIso8601String(),
       'type': _$PaymentTypeEnumMap[instance.type],
       'color': instance.color,
       'name': instance.name,
