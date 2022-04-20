62428a3af2a6b348ef8f586c872e9e39c72bd7f1
uglyonlytoday <superjang3357@naver.com>
2022-04-21 08:29:25 +0900

    add native splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,32 @@
 import 'package:dimipay/app/core/theme/color_theme.dart';
+import 'package:dimipay/app/core/utils/initializer.dart';
+import 'package:dimipay/app/data/services/auth/service.dart';
 import 'package:dimipay/app/routes/pages.dart';
 import 'package:dimipay/app/routes/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
-void main() {
+String getInintialRoute({bool debug = false}) {
+  if (debug) {
+    return Routes.TEST;
+  }
+  if (Get.find<AuthService>().token == '') {
+    return Routes.LOGIN;
+  } else {
+    return Routes.HOME;
+  }
+}
+
+Future main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  await AppInitializer().init();
+  FlutterNativeSplash.remove();
   runApp(
     GetMaterialApp(
-      initialRoute: Routes.TEST,
+      initialRoute: getInintialRoute(),
       getPages: AppPages.pages,
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
