11f136a88c4091e89ce033416a3cd1743cf3bd08
uglyonlytoday <superjang3357@naver.com>
2022-04-28 16:02:17 +0900

    Api Exception 처리

--- lib/app/data/provider/api.dart
+++ lib/app/data/provider/api.dart
@@ -1,3 +1,4 @@
+import 'package:dimipay/app/core/utils/errors.dart';
 import 'package:dimipay/app/core/utils/headers.dart';
 import 'package:dimipay/app/data/modules/coupon/model.dart';
 import 'package:dimipay/app/data/modules/event/model.dart';
@@ -12,97 +13,133 @@ class ApiProvider extends GetConnect {
     baseUrl = 'https://pay-api.dimigo.in';
   }
 
-  Future<String?> login(String username, String password) async {
+  Future<String> login(String username, String password) async {
+    String url = '/auth/login';
     Map body = {
       'username': username,
       'password': password,
     };
-    Response response = await post('/auth/login', body);
-    if (response.statusCode == 200) {
-      return response.body['accessToken'];
-    } else {
-      return null;
+    Response response = await post(url, body);
+
+    if (response.isOk == false) {
+      throw NetworkExcepton(baseUrl! + url);
     }
+
+    return response.body['accessToken'];
   }
 
   Future<User> getUserInfo() async {
-    Response response = await get('/user/me', headers: HeadersAPI().getHeaders());
+    String url = '/user/me';
+    Response response = await get(url, headers: HeadersAPI().getHeaders());
+
+    if (response.isOk == false) {
+      throw NetworkExcepton(baseUrl! + url);
+    }
+
     return User.fromJson(response.body['me']);
   }
 
   Future<List<Coupon>> getCoupons() async {
-    Response response = await get('/coupons', headers: HeadersAPI().getHeaders());
-    if (response.statusCode == 200) {
-      return response.body.map<Coupon>((model) => Coupon.fromJson(model)).toList();
-    } else {
-      return [];
+    String url = '/coupons';
+    Response response = await get(url, headers: HeadersAPI().getHeaders());
+
+    if (response.isOk == false) {
+      throw NetworkExcepton(baseUrl! + url);
     }
+
+    return (response.body as List).map<Coupon>((model) => Coupon.fromJson(model)).toList();
   }
 
-  Future<List<Notice>?> getNotice() async {
-    Response response = await get('/notice/current', headers: HeadersAPI().getHeaders());
-    if (response.statusCode == 200) {
-      return (response.body as List?)?.map((e) => Notice.fromJson(e)).toList();
-    } else {
-      return null;
+  Future<List<Notice>> getNotice() async {
+    String url = '/notice/current';
+    Response response = await get(url, headers: HeadersAPI().getHeaders());
+
+    if (response.isOk == false) {
+      throw NetworkExcepton(baseUrl! + url);
     }
+
+    return (response.body as List).map<Notice>((e) => Notice.fromJson(e)).toList();
   }
 
   Future<List<Transaction>> getTransaction() async {
-    Response response = await get('/transaction/my', headers: HeadersAPI().getHeaders());
-    if (response.statusCode == 200) {
-      return response.body.map<Transaction>((model) => Transaction.fromJson(model)).toList();
-    } else {
-      return [];
+    String url = '/transaction/my';
+    Response response = await get(url, headers: HeadersAPI().getHeaders());
+
+    if (response.isOk == false) {
+      throw NetworkExcepton(baseUrl! + url);
     }
+
+    return (response.body as List).map<Transaction>((model) => Transaction.fromJson(model)).toList();
   }
 
   Future<List<PaymentMethod>> getPaymentMethods() async {
-    Response response = await get("/payment/method", headers: HeadersAPI().getHeaders(auth: true));
-    if (response.statusCode == 200) {
-      return (response.body['paymentMethod'] as List).map((model) => PaymentMethod.fromJson(model)).toList();
-    } else {
-      return [];
+    String url = '/payment/method';
+    Response response = await get(url, headers: HeadersAPI().getHeaders(auth: true));
+
+    if (response.isOk == false) {
+      throw NetworkExcepton(baseUrl! + url);
     }
+
+    return (response.body['paymentMethod'] as List).map((model) => PaymentMethod.fromJson(model)).toList();
   }
 
   Future<void> createPaymentMethod(String cardNumber) async {
-    Map<String, String> body = {"cardNumber": cardNumber};
-    Response response = await post("/payment/method", body, headers: HeadersAPI().getHeaders(auth: true));
-    if (response.statusCode == 200) {
-      return response.body['name']; //국민카드?
+    String url = '/payment/method';
+    Map<String, String> body = {'cardNumber': cardNumber};
+    Response response = await post(url, body, headers: HeadersAPI().getHeaders(auth: true));
+
+    if (response.isOk == false) {
+      throw NetworkExcepton(baseUrl! + url);
     }
+
+    return response.body['name']; //국민카드?
   }
 
   Future<void> postPaymentToken(String token) async {
-    Map<String, String> body = {"token": token};
-    Response response = await post("/payment/token", body, headers: HeadersAPI().getHeaders(auth: true));
-    if (response.statusCode == 200) {}
+    String url = '/payment/token';
+    Map<String, String> body = {
+      'token': token,
+    };
+    Response response = await post(url, body, headers: HeadersAPI().getHeaders(auth: true));
+
+    if (response.isOk == false) {
+      throw NetworkExcepton(baseUrl! + url);
+    }
   }
 
   Future<void> createPaymentPin(String paymentPin) async {
-    Map<String, String> body = {"paymentPin": paymentPin};
-    Response response = await post("/payment/pin", body, headers: HeadersAPI().getHeaders(auth: true));
-    if (response.statusCode == 201) {
-      // 성공하면 Created
+    String url = '/payment/pin';
+    Map<String, String> body = {
+      'paymentPin': paymentPin,
+    };
+    Response response = await post(url, body, headers: HeadersAPI().getHeaders(auth: true));
+
+    if (response.isOk == false) {
+      throw NetworkExcepton(baseUrl! + url);
     }
   }
 
   Future<void> changePaymentPin(String originalPin, String paymentPin) async {
-    Map<String, String> body = {"originalPin": originalPin, "paymentPin": paymentPin};
-    Response response = await put("/payment/pin", body, headers: HeadersAPI().getHeaders(auth: true));
-    if (response.statusCode == 201) {
-      // 성공하면 Created
+    String url = '/payment/pin';
+    Map<String, String> body = {
+      'originalPin': originalPin,
+      'paymentPin': paymentPin,
+    };
+    Response response = await put(url, body, headers: HeadersAPI().getHeaders(auth: true));
 
+    if (response.isOk == false) {
+      throw NetworkExcepton(baseUrl! + url);
     }
   }
 
   Future<List<Event>> getOngoingEvents() async {
-    Response response = await get('/event/ongoing', headers: HeadersAPI().getHeaders());
-    if (response.statusCode == 200) {
-      return response.body.map<Event>((model) => Event.fromJson(model)).toList();
-    } else {
-      return [];
+    String url = '/event/ongoing';
+    Response response = await get(url, headers: HeadersAPI().getHeaders());
+
+    if (response.isOk == false) {
+      throw NetworkExcepton(baseUrl! + url);
     }
+
+    return (response.body as List).map<Event>((model) => Event.fromJson(model)).toList();
   }
 }
