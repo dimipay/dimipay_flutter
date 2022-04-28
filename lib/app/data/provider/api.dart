4d0dae5a3e64fbaaf8d15a1b9dfe60be8c1125b1
uglyonlytoday <superjang3357@naver.com>
2022-04-28 20:05:05 +0900

    rename HeadersAPI to ApiHeaderHelper

--- lib/app/data/provider/api.dart
+++ lib/app/data/provider/api.dart
@@ -26,12 +26,12 @@ class ApiProvider extends GetConnect {
   }
 
   Future<User> getUserInfo() async {
-    Response response = await get('/user/me', headers: HeadersAPI().getHeaders());
+    Response response = await get('/user/me', headers: ApiHeaderHelper().getHeaders());
     return User.fromJson(response.body['me']);
   }
 
   Future<List<Coupon>> getCoupons() async {
-    Response response = await get('/coupons', headers: HeadersAPI().getHeaders());
+    Response response = await get('/coupons', headers: ApiHeaderHelper().getHeaders());
     if (response.statusCode == 200) {
       return response.body.map<Coupon>((model) => Coupon.fromJson(model)).toList();
     } else {
@@ -40,7 +40,7 @@ class ApiProvider extends GetConnect {
   }
 
   Future<List<Notice>?> getNotice() async {
-    Response response = await get('/notice/current', headers: HeadersAPI().getHeaders());
+    Response response = await get('/notice/current', headers: ApiHeaderHelper().getHeaders());
     if (response.statusCode == 200) {
       return (response.body as List?)?.map((e) => Notice.fromJson(e)).toList();
     } else {
@@ -49,7 +49,7 @@ class ApiProvider extends GetConnect {
   }
 
   Future<List<Transaction>> getTransaction() async {
-    Response response = await get('/transaction/my', headers: HeadersAPI().getHeaders());
+    Response response = await get('/transaction/my', headers: ApiHeaderHelper().getHeaders());
     if (response.statusCode == 200) {
       return response.body.map<Transaction>((model) => Transaction.fromJson(model)).toList();
     } else {
@@ -58,7 +58,7 @@ class ApiProvider extends GetConnect {
   }
 
   Future<List<PaymentMethod>> getPaymentMethods() async {
-    Response response = await get("/payment/method", headers: HeadersAPI().getHeaders(auth: true));
+    Response response = await get("/payment/method", headers: ApiHeaderHelper().getHeaders(auth: true));
     if (response.statusCode == 200) {
       return (response.body['paymentMethod'] as List).map((model) => PaymentMethod.fromJson(model)).toList();
     } else {
@@ -68,7 +68,7 @@ class ApiProvider extends GetConnect {
 
   Future<void> createPaymentMethod(String cardNumber) async {
     Map<String, String> body = {"cardNumber": cardNumber};
-    Response response = await post("/payment/method", body, headers: HeadersAPI().getHeaders(auth: true));
+    Response response = await post("/payment/method", body, headers: ApiHeaderHelper().getHeaders(auth: true));
     if (response.statusCode == 200) {
       return response.body['name']; //국민카드?
     }
@@ -76,13 +76,13 @@ class ApiProvider extends GetConnect {
 
   Future<void> postPaymentToken(String token) async {
     Map<String, String> body = {"token": token};
-    Response response = await post("/payment/token", body, headers: HeadersAPI().getHeaders(auth: true));
+    Response response = await post("/payment/token", body, headers: ApiHeaderHelper().getHeaders(auth: true));
     if (response.statusCode == 200) {}
   }
 
   Future<void> createPaymentPin(String paymentPin) async {
     Map<String, String> body = {"paymentPin": paymentPin};
-    Response response = await post("/payment/pin", body, headers: HeadersAPI().getHeaders(auth: true));
+    Response response = await post("/payment/pin", body, headers: ApiHeaderHelper().getHeaders(auth: true));
     if (response.statusCode == 201) {
       // 성공하면 Created
     }
@@ -90,7 +90,7 @@ class ApiProvider extends GetConnect {
 
   Future<void> changePaymentPin(String originalPin, String paymentPin) async {
     Map<String, String> body = {"originalPin": originalPin, "paymentPin": paymentPin};
-    Response response = await put("/payment/pin", body, headers: HeadersAPI().getHeaders(auth: true));
+    Response response = await put("/payment/pin", body, headers: ApiHeaderHelper().getHeaders(auth: true));
     if (response.statusCode == 201) {
       // 성공하면 Created
 
@@ -98,7 +98,7 @@ class ApiProvider extends GetConnect {
   }
 
   Future<List<Event>> getOngoingEvents() async {
-    Response response = await get('/event/ongoing', headers: HeadersAPI().getHeaders());
+    Response response = await get('/event/ongoing', headers: ApiHeaderHelper().getHeaders());
     if (response.statusCode == 200) {
       return response.body.map<Event>((model) => Event.fromJson(model)).toList();
     } else {
