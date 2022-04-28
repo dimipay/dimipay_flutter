14941db3a486a9b1b717d600b11db514fee2c533
uglyonlytoday <superjang3357@naver.com>
2022-04-28 15:24:25 +0900

    .

--- lib/app/pages/event/binding.dart
+++ lib/app/pages/event/binding.dart
@@ -1,6 +1,6 @@
 import 'package:dimipay/app/data/provider/api.dart';
-import 'package:dimipay/app/pages/event/controller.dart';
-import 'package:dimipay/app/pages/event/repository.dart';
+import 'package:dimipay/app/data/modules/event/controller.dart';
+import 'package:dimipay/app/data/modules/event/repository.dart';
 import 'package:get/get.dart';
 
 class EventPageBinding implements Bindings {
