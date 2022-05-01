76dea962a17799e8c17304c21c4f4f45c5eacc41
uglyonlytoday <superjang3357@naver.com>
2022-05-02 08:32:18 +0900

    add ErrorSnackbar when login

--- lib/app/pages/login/controller.dart
+++ lib/app/pages/login/controller.dart
@@ -1,5 +1,7 @@
+import 'package:dimipay/app/core/utils/errors.dart';
 import 'package:dimipay/app/data/services/auth/service.dart';
 import 'package:dimipay/app/routes/routes.dart';
+import 'package:dimipay/app/widgets/snackbar.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -15,11 +17,15 @@ class LoginPageController extends GetxController with StateMixin {
   }
 
   Future login() async {
-    change(null, status: RxStatus.loading());
-    await authService.login(usernameField.text, passwordField.text);
-    change(null, status: RxStatus.success());
-    if (authService.isAuthenticated) {
-      Get.offNamed(Routes.HOME);
+    try {
+      change(null, status: RxStatus.loading());
+      await authService.login(usernameField.text, passwordField.text);
+      if (authService.isAuthenticated) {
+        Get.offNamed(Routes.HOME);
+      }
+    } on NetworkExcepton catch (e) {
+      DPErrorSnackBar().open(e.toString());
     }
+    change(null, status: RxStatus.success());
   }
 }
