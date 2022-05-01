ac82b02135870ba6dc5e79805334343e81a44355
uglyonlytoday <superjang3357@naver.com>
2022-05-02 08:32:03 +0900

    add HapticHelper

---
+++ lib/app/core/utils/haptic.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/services.dart';
+
+class HapticPattern {
+  final List<int> ticks;
+  HapticPattern(this.ticks);
+}
+
+class HapticPatterns {
+  static final error = HapticPattern([0, 150, 300]);
+  static final transactionSuccess = HapticPattern([0, 500]);
+}
+
+abstract class HapticHelper {
+  static Future feedback(HapticPattern pattern) async {
+    List<Future> tasks = [];
+
+    for (var tick in pattern.ticks) {
+      tasks.add(Future.delayed(Duration(milliseconds: tick), HapticFeedback.heavyImpact));
+    }
+
+    await Future.wait(tasks);
+  }
+}
