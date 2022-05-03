25747dc099b689d98fb19a74b9137ca12e1ff40c
uglyonlytoday <superjang3357@naver.com>
2022-05-03 18:02:04 +0900

    페이지 구현

--- lib/app/pages/transaction_fin/page.dart
+++ lib/app/pages/transaction_fin/page.dart
@@ -1,12 +1,41 @@
+import 'package:dimipay/app/core/theme/text_theme.dart';
+import 'package:dimipay/app/widgets/button.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
 
 class TransactionFinPage extends StatelessWidget {
+  const TransactionFinPage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('TransactionFinPage')),
-      body: Container(),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Lottie.asset('asset/lottie/complete.json', height: 240),
+                    const Text('성공적으로 결제되었어요', style: DPTextTheme.PAGE_HEADER),
+                  ],
+                ),
+              ),
+              DPMediumTextButton(
+                text: '확인',
+                width: double.infinity,
+                onTap: () {
+                  Get.back();
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
