f928eeb3bc560303556fef02676a40abf7db1046
uglyonlytoday <superjang3357@naver.com>
2022-05-09 16:37:17 +0900

    refactor DPDialog

--- lib/app/widgets/dialog.dart
+++ lib/app/widgets/dialog.dart
@@ -2,7 +2,6 @@ import 'package:dimipay/app/core/theme/color_theme.dart';
 import 'package:dimipay/app/core/theme/text_theme.dart';
 import 'package:dimipay/app/widgets/button.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
 class DPDialogAction {
   final String title;
@@ -25,43 +24,6 @@ class DPDialogActionButton extends StatelessWidget {
   }
 }
 
-class DPBaseDialog extends StatelessWidget {
-  const DPBaseDialog({Key? key, required this.title, required this.content, required this.button}) : super(key: key);
-  final String title;
-  final Widget content;
-  final Widget button;
-
-  @override
-  Widget build(BuildContext context) {
-    return Dialog(
-      backgroundColor: Colors.transparent,
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(12),
-          color: Colors.white,
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(24),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Text(
-                title,
-                style: DPTextTheme.READABLE,
-              ),
-              const SizedBox(height: 12),
-              content,
-              const SizedBox(height: 24),
-              button,
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
 class DPDialog extends StatelessWidget {
   const DPDialog({Key? key, required this.title, required this.content, required this.actions}) : super(key: key);
 
@@ -73,34 +35,25 @@ class DPDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Dialog(
-      backgroundColor: Colors.transparent,
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(12),
-          color: Colors.white,
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(24),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Text(
-                title,
-                style: DPTextTheme.READABLE,
+      shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(12))),
+      child: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(title, style: DPTextTheme.READABLE),
+            const SizedBox(height: 12),
+            Text(content, style: DPTextTheme.DESCRIPTION),
+            const SizedBox(height: 12),
+            for (var action in actions)
+              Column(
+                children: [
+                  const SizedBox(height: 12),
+                  DPDialogActionButton(action),
+                ],
               ),
-              const SizedBox(height: 12),
-              Text(content, style: DPTextTheme.DESCRIPTION),
-              const SizedBox(height: 12),
-              for (var action in actions)
-                Column(
-                  children: [
-                    const SizedBox(height: 12),
-                    DPDialogActionButton(action),
-                  ],
-                ),
-            ],
-          ),
+          ],
         ),
       ),
     );
