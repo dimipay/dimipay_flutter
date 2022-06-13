67eeddfcb434caae8490f4d07af7f48366c713d2
uglyonlytoday <superjang3357@naver.com>
2022-06-13 21:02:27 +0900

    add loginPage id, password validator

--- lib/app/pages/login/controller.dart
+++ lib/app/pages/login/controller.dart
@@ -4,8 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class LoginPageController extends GetxController with StateMixin {
-  TextEditingController usernameField = TextEditingController();
-  TextEditingController passwordField = TextEditingController();
+  final TextEditingController usernameField = TextEditingController();
+  final TextEditingController passwordField = TextEditingController();
+  final formKey = GlobalKey<FormState>();
   AuthService authService = Get.find<AuthService>();
 
   @override
@@ -14,12 +15,35 @@ class LoginPageController extends GetxController with StateMixin {
     super.onInit();
   }
 
-  Future login() async {
-    change(null, status: RxStatus.loading());
-    await authService.login(usernameField.text, passwordField.text);
-    change(null, status: RxStatus.success());
-    if (authService.isAuthenticated) {
-      Get.offNamed(Routes.HOME);
+  String? idValidator(String? id) {
+    if (id == null || id.isEmpty) {
+      return '아이디를 입력하세요';
     }
+    return null;
+  }
+
+  String? passwordValidator(String? password) {
+    if (password == null || password.isEmpty) {
+      return '바밀번호를 입력하세요';
+    }
+    return null;
+  }
+
+  Future login() async {
+    // try {
+    if (formKey.currentState!.validate()) {
+      formKey.currentState!.save();
+      change(null, status: RxStatus.loading());
+      await authService.login(usernameField.text, passwordField.text);
+      change(null, status: RxStatus.success());
+      if (authService.isAuthenticated) {
+        Get.offNamed(Routes.HOME);
+      }
+    }
+
+    // } catch (e) {
+    //   change(null, status: RxStatus.error());
+    //   throw e;
+    // }
   }
 }
