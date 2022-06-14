151b1d35f0e6890fa1c2a02f14d404d88c7aea8f
uglyonlytoday <superjang3357@naver.com>
2022-06-14 21:52:42 +0900

    Dio로 httpclient 변경

--- lib/app/data/provider/api.dart
+++ lib/app/data/provider/api.dart
@@ -1,17 +1,157 @@
-import 'package:dimipay/app/core/utils/errors.dart';
 import 'package:dimipay/app/data/modules/coupon/model.dart';
 import 'package:dimipay/app/data/modules/event/model.dart';
 import 'package:dimipay/app/data/modules/notice/model.dart';
 import 'package:dimipay/app/data/modules/payment_method/model.dart';
 import 'package:dimipay/app/data/modules/transaction/model.dart';
 import 'package:dimipay/app/data/modules/user/model.dart';
-import 'package:dimipay/app/core/utils/headers.dart';
 import 'package:dimipay/app/data/provider/api_interface.dart';
-import 'package:get/get.dart';
+import 'package:dimipay/app/data/services/auth/service.dart';
+import 'package:dio/dio.dart';
+import 'package:get/instance_manager.dart';
+import 'dart:developer';
 
-class ApiProvider extends GetConnect implements ApiInterface {
+class JWTInterceptor extends Interceptor {
+  @override
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+    AuthService authService = Get.find<AuthService>();
+    String? token = authService.token;
+    if (token != null) {
+      options.headers['Authorization'] = 'Bearer $token';
+    }
+    handler.next(options);
+  }
+}
+
+class LogInterceptor extends Interceptor {
+  @override
+  void onResponse(Response response, ResponseInterceptorHandler handler) {
+    log('RESPONSE[${response.statusCode}] => PATH: ${response.requestOptions.path}', name: 'DIO');
+    handler.next(response);
+  }
+}
+
+class ApiProvider implements ApiInterface {
+  final Dio dio = Dio();
+  final baseUrl = 'http://dimipay-api-dev.rycont.ninja';
+
   ApiProvider() {
-    baseUrl = 'http://dimipay-api-dev.rycont.ninja';
+    dio.options.baseUrl = baseUrl;
+    dio.interceptors.add(JWTInterceptor());
+    dio.interceptors.add(LogInterceptor());
+  }
+
+  @override
+  Future<void> changePaymentPin(String originalPin, String paymentPin) async {
+    String url = '/payment/pin';
+    Map<String, String> body = {
+      'originalPin': originalPin,
+      'paymentPin': paymentPin,
+    };
+    await dio.put(url, data: body);
+
+    // if (response.isOk == false) {
+    //   throw NetworkExcepton(baseUrl! + url, code: response.statusCode!);
+    // }
+  }
+
+  @override
+  Future<void> createPaymentMethod(String cardNumber) async {
+    String url = '/payment/method';
+    Map<String, String> body = {'cardNumber': cardNumber};
+    Response response = await dio.post(url, data: body);
+
+    // if (response.isOk == false) {
+    //   throw NetworkExcepton(baseUrl! + url, code: response.statusCode!);
+    // }
+
+    return response.data['name']; //국민카드?
+  }
+
+  @override
+  Future<void> createPaymentPin(String paymentPin) async {
+    String url = '/payment/pin';
+    Map<String, String> body = {
+      'paymentPin': paymentPin,
+    };
+    await dio.post(url, data: body);
+
+    // if (response.isOk == false) {
+    //   throw NetworkExcepton(baseUrl! + url, code: response.statusCode!);
+    // }
+  }
+
+  @override
+  Future<List<Coupon>> getCoupons() async {
+    String url = '/coupons';
+
+    Response response = await dio.get(url);
+
+    // if (response.isOk == false) {
+    //   throw NetworkExcepton(baseUrl! + url, code: response.statusCode!);
+    // }
+
+    return (response.data as List).map<Coupon>((model) => Coupon.fromJson(model)).toList();
+  }
+
+  @override
+  Future<List<Notice>> getNotice() async {
+    String url = '/notice/current';
+    Response response = await dio.get(url);
+
+    // if (response.isOk == false) {
+    //   throw NetworkExcepton(baseUrl! + url, code: response.statusCode!);
+    // }
+
+    return (response.data as List).map<Notice>((e) => Notice.fromJson(e)).toList();
+  }
+
+  @override
+  Future<List<Event>> getOngoingEvents() async {
+    String url = '/event/ongoing';
+    Response response = await dio.get(url);
+
+    // if (response.isOk == false) {
+    //   throw NetworkExcepton(baseUrl! + url, code: response.statusCode!);
+    // }
+
+    return (response.data as List).map<Event>((model) => Event.fromJson(model)).toList();
+  }
+
+  @override
+  Future<List<PaymentMethod>> getPaymentMethods() async {
+    String url = '/payment/method';
+    Response response = await dio.get(url);
+
+    // if (response.isOk == false) {
+    //   throw NetworkExcepton(baseUrl! + url, code: response.statusCode!);
+    // }
+
+    return (response.data['paymentMethod'] as List?)?.map((model) => PaymentMethod.fromJson(model)).toList() ?? [];
+  }
+
+  @override
+  Future<List<Transaction>> getTransaction() async {
+    String url = '/transaction/my';
+    Response response = await dio.get(url);
+
+    // if (response.isOk == false) {
+    //   throw NetworkExcepton(baseUrl! + url, code: response.statusCode!);
+    // }
+
+    return (response.data as List).map<Transaction>((model) => Transaction.fromJson(model)).toList();
+  }
+
+  @override
+  Future<User> getUserInfo() async {
+    String url = '/user/me';
+
+    Response response = await dio.get(url);
+
+    // if (response.isOk == false) {
+    //   throw NetworkExcepton(baseUrl! + url, code: response.statusCode!);
+    // }
+
+    return User.fromJson(response.data['me']);
   }
 
   @override
@@ -24,89 +164,17 @@ class ApiProvider extends GetConnect implements ApiInterface {
       "deviceUid": "9cf20349-c0af-42d3-820e-f17862c2d923",
       "bioKey": "580a4559-78c7-4d00-a5b7-b501b2c0e112",
     };
-    Response response = await post(url, body);
 
-    if (response.isOk == false) {
-      if (response.statusCode == 401) {
-        throw NetworkExcepton(response.body['message'], code: response.statusCode!);
-      }
-      throw NetworkExcepton(baseUrl! + url, code: response.statusCode!);
-    }
+    Response response = await dio.post(url, data: body);
 
-    return response.body['accessToken'];
-  }
+    // if (response.statusCode == 200) {
+    //   if (response.statusCode == 401) {
+    //     throw NetworkExcepton(response.body['message'], code: response.statusCode!);
+    //   }
+    //   throw NetworkExcepton(baseUrl + url, code: response.statusCode!);
+    // }
 
-  @override
-  Future<User> getUserInfo() async {
-    String url = '/user/me';
-    Response response = await get(url, headers: ApiHeaderHelper().getHeaders());
-
-    if (response.isOk == false) {
-      throw NetworkExcepton(baseUrl! + url, code: response.statusCode!);
-    }
-
-    return User.fromJson(response.body['me']);
-  }
-
-  @override
-  Future<List<Coupon>> getCoupons() async {
-    String url = '/coupons';
-    Response response = await get(url, headers: ApiHeaderHelper().getHeaders());
-
-    if (response.isOk == false) {
-      throw NetworkExcepton(baseUrl! + url, code: response.statusCode!);
-    }
-
-    return (response.body as List).map<Coupon>((model) => Coupon.fromJson(model)).toList();
-  }
-
-  @override
-  Future<List<Notice>> getNotice() async {
-    String url = '/notice/current';
-    Response response = await get(url, headers: ApiHeaderHelper().getHeaders());
-
-    if (response.isOk == false) {
-      throw NetworkExcepton(baseUrl! + url, code: response.statusCode!);
-    }
-
-    return (response.body as List).map<Notice>((e) => Notice.fromJson(e)).toList();
-  }
-
-  @override
-  Future<List<Transaction>> getTransaction() async {
-    String url = '/transaction/my';
-    Response response = await get(url, headers: ApiHeaderHelper().getHeaders());
-
-    if (response.isOk == false) {
-      throw NetworkExcepton(baseUrl! + url, code: response.statusCode!);
-    }
-
-    return (response.body as List).map<Transaction>((model) => Transaction.fromJson(model)).toList();
-  }
-
-  @override
-  Future<List<PaymentMethod>> getPaymentMethods() async {
-    String url = '/payment/method';
-    Response response = await get(url, headers: ApiHeaderHelper().getHeaders(auth: true));
-
-    if (response.isOk == false) {
-      throw NetworkExcepton(baseUrl! + url, code: response.statusCode!);
-    }
-
-    return (response.body['paymentMethod'] as List?)?.map((model) => PaymentMethod.fromJson(model)).toList() ?? [];
-  }
-
-  @override
-  Future<void> createPaymentMethod(String cardNumber) async {
-    String url = '/payment/method';
-    Map<String, String> body = {'cardNumber': cardNumber};
-    Response response = await post(url, body, headers: ApiHeaderHelper().getHeaders(auth: true));
-
-    if (response.isOk == false) {
-      throw NetworkExcepton(baseUrl! + url, code: response.statusCode!);
-    }
-
-    return response.body['name']; //국민카드?
+    return response.data['accessToken'];
   }
 
   @override
@@ -115,49 +183,24 @@ class ApiProvider extends GetConnect implements ApiInterface {
     Map<String, String> body = {
       'token': token,
     };
-    Response response = await post(url, body, headers: ApiHeaderHelper().getHeaders(auth: true));
+    await dio.post(url, data: body);
 
-    if (response.isOk == false) {
-      throw NetworkExcepton(baseUrl! + url, code: response.statusCode!);
-    }
+    // if (response.isOk == false) {
+    //   throw NetworkExcepton(baseUrl! + url, code: response.statusCode!);
+    // }
   }
 
-  @override
-  Future<void> createPaymentPin(String paymentPin) async {
-    String url = '/payment/pin';
-    Map<String, String> body = {
-      'paymentPin': paymentPin,
-    };
-    Response response = await post(url, body, headers: ApiHeaderHelper().getHeaders(auth: true));
-
-    if (response.isOk == false) {
-      throw NetworkExcepton(baseUrl! + url, code: response.statusCode!);
-    }
-  }
-
-  @override
-  Future<void> changePaymentPin(String originalPin, String paymentPin) async {
-    String url = '/payment/pin';
-    Map<String, String> body = {
-      'originalPin': originalPin,
-      'paymentPin': paymentPin,
-    };
-    Response response = await put(url, body, headers: ApiHeaderHelper().getHeaders(auth: true));
-
-    if (response.isOk == false) {
-      throw NetworkExcepton(baseUrl! + url, code: response.statusCode!);
-    }
-  }
-
-  @override
-  Future<List<Event>> getOngoingEvents() async {
-    String url = '/event/ongoing';
-    Response response = await get(url, headers: ApiHeaderHelper().getHeaders());
-
-    if (response.isOk == false) {
-      throw NetworkExcepton(baseUrl! + url, code: response.statusCode!);
-    }
-
-    return (response.body as List).map<Event>((model) => Event.fromJson(model)).toList();
-  }
+  // getAll() async {
+  //   try {
+  //     var response = await dio.get(baseUrl);
+  //     if (response.statusCode == 200) {
+  //       Map<String, dynamic> jsonResponse = json.decode(response.data);
+  //       List<MyModel> listMyModel = jsonResponse['data'].map<MyModel>((map) {
+  //         return MyModel.fromJson(map);
+  //       }).toList();
+  //       return listMyModel;
+  //     } else
+  //       print('erro -get');
+  //   } catch (_) {}
+  // }
 }
