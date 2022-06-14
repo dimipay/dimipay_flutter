c0971c2fb25197a61faf0054cb8d91422f580bce
uglyonlytoday <superjang3357@naver.com>
2022-06-14 22:46:34 +0900

    schema 수정

--- lib/app/data/modules/payment_method/model.dart
+++ lib/app/data/modules/payment_method/model.dart
@@ -1,26 +1,24 @@
-// ignore_for_file: constant_identifier_names
-
 import 'package:json_annotation/json_annotation.dart';
 part 'model.g.dart';
 
 // g.dart 파일 생성 : flutter pub run build_runner build --delete-conflicting-outputs
-
-enum PaymentType {
-  PREPAID,
-  GENERAL,
-  COUPON,
-}
 
 @JsonSerializable()
 class PaymentMethod {
   String systemId;
   DateTime createdAt;
   DateTime updatedAt;
-  PaymentType type;
   String color;
   String name;
   int ownerId;
-  PaymentMethod({required this.systemId, required this.createdAt, required this.updatedAt, required this.type, required this.color, required this.name, required this.ownerId});
+  PaymentMethod({
+    required this.systemId,
+    required this.createdAt,
+    required this.updatedAt,
+    required this.color,
+    required this.name,
+    required this.ownerId,
+  });
 
   factory PaymentMethod.fromJson(Map<String, dynamic> json) => _$PaymentMethodFromJson(json);
   Map<String, dynamic> toJson() => _$PaymentMethodToJson(this);
