1f2049fe82324322bae376567db1992290063f25
uglyonlytoday <superjang3357@naver.com>
2022-06-15 10:48:26 +0900

    view에 paymentMethod를 보여줌

--- lib/app/data/modules/payment_method/repository.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dimipay/app/data/modules/payment_method/model.dart';
-import 'package:dimipay/app/data/provider/api_interface.dart';
-
-class PaymentMethodRepository {
-  final ApiInterface api;
-  PaymentMethodRepository(this.api);
-  Future<List<PaymentMethod>> get() => api.getPaymentMethods();
-}
--- lib/app/pages/home/binding.dart
+++ lib/app/pages/home/binding.dart
@@ -2,8 +2,10 @@ import 'package:dimipay/app/data/modules/coupon/controller.dart';
 import 'package:dimipay/app/data/modules/coupon/repository.dart';
 import 'package:dimipay/app/data/modules/notice/controller.dart';
 import 'package:dimipay/app/data/modules/notice/repository.dart';
-import 'package:dimipay/app/data/modules/payment_method/controller.dart';
-import 'package:dimipay/app/data/modules/payment_method/repository.dart';
+import 'package:dimipay/app/data/modules/payment_method/general/controller.dart';
+import 'package:dimipay/app/data/modules/payment_method/general/repository.dart';
+import 'package:dimipay/app/data/modules/payment_method/prepaid/controller.dart';
+import 'package:dimipay/app/data/modules/payment_method/prepaid/repository.dart';
 import 'package:dimipay/app/data/modules/user/controller.dart';
 import 'package:dimipay/app/data/modules/user/repository.dart';
 import 'package:dimipay/app/data/provider/api.dart';
@@ -16,8 +18,9 @@ class HomePageBinding implements Bindings {
   void dependencies() {
     Get.put<UserController>(UserController(UserRepository(ApiProvider())));
     Get.put<CouponController>(CouponController(CouponRepository(ApiProvider())));
-    Get.lazyPut<NoticeController>(() => NoticeController(NoticeRepository(ApiProvider())));
     Get.put<EventController>(EventController(EventRepository(ApiProvider())));
-    Get.put<PaymentMethodsController>(PaymentMethodsController(PaymentMethodRepository(ApiProvider())));
+    Get.put<PrepaidCardController>(PrepaidCardController(PrepaidCardRepository(ApiProvider())));
+    Get.put<GeneralCardController>(GeneralCardController(GeneralCardRepository(ApiProvider())));
+    Get.lazyPut<NoticeController>(() => NoticeController(NoticeRepository(ApiProvider())));
   }
 }
