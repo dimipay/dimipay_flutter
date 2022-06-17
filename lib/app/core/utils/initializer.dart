55fd4ca779a6dd701acbd5af7a4e549679846289
uglyonlytoday <superjang3357@naver.com>
2022-06-17 21:08:16 +0900

    코드 정리

--- lib/app/core/utils/initializer.dart
+++ lib/app/core/utils/initializer.dart
@@ -2,12 +2,10 @@ import 'package:dimipay/app/data/provider/api.dart';
 import 'package:dimipay/app/data/services/auth/repository.dart';
 import 'package:dimipay/app/data/services/auth/service.dart';
 import 'package:dimipay/app/data/services/config/service.dart';
-import 'package:dimipay/app/data/services/first/first.dart';
 import 'package:get/get.dart';
 
 class AppInitializer {
   Future<void> init() async {
-    await Get.putAsync<IsFirstTime>(() => IsFirstTime().init());
     await Get.putAsync<AuthService>(() => AuthService(AuthRepository(ApiProvider())).init());
     await Get.putAsync<AppConfigService>(() => AppConfigService().init());
   }
