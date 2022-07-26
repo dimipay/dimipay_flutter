76f9f2cd2b072c06ce2ba6965c84870d4eb6b19f
Minyong Oh <dhalsdyd@gmail.com>
2022-07-26 19:24:13 +0900

    FEAT : 불필요한 코드 제거

--- lib/app/data/provider/google_signin_api.dart
+++ lib/app/data/provider/google_signin_api.dart
@@ -1,13 +1,14 @@
 import 'package:google_sign_in/google_sign_in.dart';
 
 class GoogleSignInAPI {
-  static final GoogleSignIn _googleSignIn = GoogleSignIn();
-  static Future<GoogleSignInAccount?> login() => _googleSignIn.signIn();
-  static Future logout() => _googleSignIn.signOut();
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  Future<GoogleSignInAccount?> login() => _googleSignIn.signIn();
+  Future logout() => _googleSignIn.signOut();
 
-  static Future<String?> getIdToken() async {
-    final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
-    final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
-    return googleAuth!.idToken;
+  Future<String> getIdToken() async {
+    await logout();
+    final GoogleSignInAccount googleUser = (await _googleSignIn.signIn())!;
+    final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+    return googleAuth.idToken!;
   }
 }
