6bd3f81041575df8b41572249757132dfa861d9b
Minyong Oh <dhalsdyd@gmail.com>
2022-07-26 19:05:51 +0900

    FEAT : 구글 로그인 기능 추가

--- lib/app/core/utils/initializer.dart
+++ lib/app/core/utils/initializer.dart
@@ -1,4 +1,5 @@
 import 'package:dimipay/app/data/provider/api.dart';
+import 'package:dimipay/app/data/provider/google_signin_api.dart';
 import 'package:dimipay/app/data/services/auth/repository.dart';
 import 'package:dimipay/app/data/services/auth/service.dart';
 import 'package:dimipay/app/data/services/config/service.dart';
@@ -6,7 +7,7 @@ import 'package:get/get.dart';
 
 class AppInitializer {
   Future<void> init() async {
-    await Get.putAsync<AuthService>(() => AuthService(AuthRepository(ApiProvider())).init());
+    await Get.putAsync<AuthService>(() => AuthService(AuthRepository(ApiProvider(), GoogleSignInAPI())).init());
     await Get.putAsync<AppConfigService>(() => AppConfigService().init());
   }
 }
