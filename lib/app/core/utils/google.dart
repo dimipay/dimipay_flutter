8698246ee4887c67647e9e63f5507f48d98f82c6
uglyonlytoday <superjang3357@naver.com>
2022-08-04 01:08:53 +0900

    fix : web google sign-in

--- lib/app/core/utils/google.dart
+++ lib/app/core/utils/google.dart
@@ -2,14 +2,12 @@ import 'package:google_sign_in/google_sign_in.dart';
 
 class GoogleSignInHelper {
   final GoogleSignIn _googleSignIn = GoogleSignIn();
-  Future<GoogleSignInAccount?> login() => _googleSignIn.signIn();
-  Future logout() => _googleSignIn.signOut();
 
   ///returns google idToken
   ///중간에 로그인이 취소 될 시 exception 발생
   Future<String> authenticate({bool selectAccount = true}) async {
     if (selectAccount) {
-      await logout();
+      await _googleSignIn.disconnect();
     }
     final GoogleSignInAccount? googleAccount = await _googleSignIn.signIn();
     if (googleAccount == null) {
