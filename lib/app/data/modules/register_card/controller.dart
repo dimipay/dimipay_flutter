c1c92c3de41ca595a917af7df5650e75d3a2d0eb
Minyong Oh <dhalsdyd@gmail.com>
2022-08-10 20:49:26 +0900

    FEAT : add registerGeneralCard func

--- lib/app/data/modules/register_card/controller.dart
+++ lib/app/data/modules/register_card/controller.dart
@@ -1,9 +1,23 @@
+import 'package:dimipay/app/data/modules/register_card/repository.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class RegisterCardPageController extends GetxController {
+  final RegisterCardRepository repository;
+  RegisterCardPageController(this.repository);
+
   TextEditingController? cardNumberFieldController;
   TextEditingController? expireDateFieldController;
   TextEditingController? birthdayFieldController;
   TextEditingController? passwordFieldController;
+
+  Future<void> createGeneralCard() async {
+    String cardNumber = cardNumberFieldController!.text;
+    String expireDate = expireDateFieldController!.text;
+    String birthday = birthdayFieldController!.text;
+    String password = passwordFieldController!.text;
+    String year = expireDate.substring(0, 2);
+    String month = expireDate.substring(2);
+    await repository.createGeneralCard(cardNumber, year, month, birthday, password);
+  }
 }
--- lib/app/pages/register_card/binding.dart
+++ lib/app/pages/register_card/binding.dart
@@ -1,9 +1,11 @@
 import 'package:dimipay/app/data/modules/register_card/controller.dart';
+import 'package:dimipay/app/data/modules/register_card/repository.dart';
+import 'package:dimipay/app/data/provider/api.dart';
 import 'package:get/get.dart';
 
 class RegisterCardPageBinding implements Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<RegisterCardPageController>(() => RegisterCardPageController());
+    Get.lazyPut<RegisterCardPageController>(() => RegisterCardPageController(RegisterCardRepository(ApiProvider())));
   }
 }
