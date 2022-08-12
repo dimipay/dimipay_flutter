665d8b7aa44b83617f3501fb36963c4e6dec54e1
Minyong Oh <dhalsdyd@gmail.com>
2022-08-12 20:30:31 +0900

    FEAT : add FormValidation and StateMixin

--- lib/app/pages/register_card/controller.dart
+++ lib/app/pages/register_card/controller.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:credit_card_scanner/credit_card_scanner.dart';
 import 'package:dimipay/app/data/modules/payment_method/controller.dart';
 import 'package:dimipay/app/widgets/snackbar.dart';
@@ -5,13 +7,81 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class RegisterCardPageController extends GetxController {
+class RegisterCardPageController extends GetxController with StateMixin {
   final PaymentMethodController paymentMethodController = Get.find<PaymentMethodController>();
 
+  final formKey = GlobalKey<FormState>();
+  final Rx<String> cardNumber = Rx("");
+  final Rx<String> expiredDate = Rx("");
+  final Rx<String> birthday = Rx("");
+  final Rx<String> password = Rx("");
+
+  @override
+  void onInit() {
+    change(null, status: RxStatus.success());
+    super.onInit();
+  }
+
+  bool get buttonEnabled {
+    if (cardNumberValidator(cardNumber.value) != null) {
+      return false;
+    }
+    if (expiredDateValidator(expiredDate.value) != null) {
+      return false;
+    }
+    if (birthdayValidator(birthday.value) != null) {
+      return false;
+    }
+    if (passwordValidator(password.value) != null) {
+      return false;
+    }
+    return true;
+  }
+
+  String? cardNumberValidator(String? cardNumber) {
+    if (cardNumber == null || cardNumber.isEmpty) {
+      return "카드 번호를 입력하세요";
+    }
+    if (cardNumber.length != 16) {
+      return "카드 번호 16자리를 입력해주세요";
+    }
+    return null;
+  }
+
+  String? expiredDateValidator(String? expiredDate) {
+    if (expiredDate == null || expiredDate.isEmpty) {
+      return "만료기한를 입력하세요";
+    }
+    if (expiredDate.length != 4) {
+      return "만료기한 4자리를 입력해주세요";
+    }
+    return null;
+  }
+
+  String? birthdayValidator(String? birthday) {
+    if (birthday == null || birthday.isEmpty) {
+      return "생년월일를 입력하세요";
+    }
+    if (birthday.length != 6) {
+      return "생년월일 6자리를 입력해주세요";
+    }
+    return null;
+  }
+
+  String? passwordValidator(String? password) {
+    if (password == null || password.isEmpty) {
+      return "카드 비밀번호 앞 2자리를 입력하세요";
+    }
+    if (password.length != 2) {
+      return "카드 비밀번호 앞 2자리를 입력해주세요";
+    }
+    return null;
+  }
+
   final TextEditingController cardNumberFieldController = TextEditingController();
-  final TextEditingController expireDateFieldController = TextEditingController();
-  final TextEditingController birthdayFieldController = TextEditingController();
-  final TextEditingController passwordFieldController = TextEditingController();
+  final TextEditingController expiredDateFieldController = TextEditingController();
+  //final TextEditingController birthdayFieldController = TextEditingController();
+  //final TextEditingController passwordFieldController = TextEditingController();
 
   Future<void> scanCreditCard() async {
     final CardDetails? cardInfo = await CardScanner.scanCard(
@@ -20,28 +90,38 @@ class RegisterCardPageController extends GetxController {
       ),
     );
 
+    print(cardInfo!.map);
+
     if (cardInfo != null) {
       cardNumberFieldController.text = cardInfo.cardNumber;
-      expireDateFieldController.text = cardInfo.expiryDate.replaceAll("/", "");
+      expiredDateFieldController.text = cardInfo.expiryDate.replaceAll("/", "");
     }
   }
 
   Future<void> createGeneralCard() async {
-    String cardNumber = cardNumberFieldController.text.replaceAll('-', '');
-    String password = passwordFieldController.text;
+    //String cardNumber = cardNumberFieldController.text.replaceAll('-', '');
+    //String password = passwordFieldController.text;
     // ignore: prefer_interpolation_to_compose_strings
-    DateTime ownerBirthday = DateTime.parse('00' + birthdayFieldController.text);
-    DateTime expireAt = DateTime(int.parse(expireDateFieldController.text.substring(2)), int.parse(expireDateFieldController.text.substring(0, 2)));
+    //DateTime ownerBirthday = DateTime.parse('00' + birthdayFieldController.text);
+    //DateTime expireAt = DateTime(int.parse(expireDateFieldController.text.substring(2)), int.parse(expireDateFieldController.text.substring(0, 2)));
     try {
-      await paymentMethodController.createGeneralCard(
-        cardNumber: cardNumber,
-        password: password,
-        ownerBirthday: ownerBirthday,
-        expireAt: expireAt,
-      );
-      Get.back();
+      if (formKey.currentState!.validate()) {
+        formKey.currentState!.save();
+        change(null, status: RxStatus.loading());
+        await paymentMethodController.createGeneralCard(
+          cardNumber: cardNumber.value,
+          password: password.value,
+          ownerBirthday: DateTime.parse("00${birthday.value}"),
+          expireAt: DateTime(int.parse(expiredDate.value.substring(2)), int.parse(expiredDate.value.substring(0, 2))),
+        );
+
+        Get.back();
+      }
     } on DioError catch (e) {
+      log(e.response.toString());
       DPErrorSnackBar().open(e.response!.data['message']);
+    } finally {
+      change(null, status: RxStatus.success());
     }
   }
 }
--- lib/app/pages/register_card/page.dart
+++ lib/app/pages/register_card/page.dart
@@ -20,66 +20,97 @@ class RegisterCardPage extends GetView<RegisterCardPageController> {
               child: SingleChildScrollView(
                 child: Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 24),
-                  child: Column(
-                    children: [
-                      DPSmallTextButton(
-                          text: "스캐너",
-                          onTap: () {
-                            controller.scanCreditCard();
-                          }),
-                      const SizedBox(height: 32),
-                      DPTextField(
-                        controller: controller.cardNumberFieldController,
-                        label: '카드 번호',
-                        textInputType: TextInputType.text,
-                        maxLength: 16,
-                        hintText: '카드 번호 16자리를 입력해주세요',
-                      ),
-                      const SizedBox(height: 18),
-                      Row(
-                        children: [
-                          Flexible(
-                            child: DPTextField(
-                              controller: controller.expireDateFieldController,
-                              label: '유효기간',
-                              maxLength: 4,
-                              textInputType: TextInputType.number,
-                              hintText: 'MM/YY',
+                  child: Form(
+                    key: controller.formKey,
+                    child: Column(
+                      children: [
+                        DPSmallTextButton(
+                            text: "스캐너",
+                            onTap: () {
+                              controller.scanCreditCard();
+                            }),
+                        const SizedBox(height: 32),
+                        DPTextFormField(
+                          controller: controller.cardNumberFieldController,
+                          validator: controller.cardNumberValidator,
+                          onChanged: (cardNumber) {
+                            controller.cardNumber.value = cardNumber;
+                          },
+                          textInputAction: TextInputAction.next,
+                          label: '카드 번호',
+                          textInputType: TextInputType.number,
+                          maxLength: 16,
+                          hintText: '카드 번호 16자리를 입력해주세요',
+                        ),
+                        const SizedBox(height: 18),
+                        Row(
+                          children: [
+                            Flexible(
+                              child: DPTextFormField(
+                                controller: controller.expiredDateFieldController,
+                                validator: controller.expiredDateValidator,
+                                onChanged: (expiredDate) {
+                                  controller.expiredDate.value = expiredDate;
+                                },
+                                textInputAction: TextInputAction.next,
+                                label: '유효기간',
+                                maxLength: 4,
+                                textInputType: TextInputType.number,
+                                hintText: 'MM/YY',
+                              ),
                             ),
-                          ),
-                          const SizedBox(width: 18),
-                          Flexible(
-                            child: DPTextField(
-                              controller: controller.birthdayFieldController,
-                              label: '생년 월일',
-                              maxLength: 6,
-                              textInputType: TextInputType.number,
-                              hintText: '6자리로 입력',
+                            const SizedBox(width: 18),
+                            Flexible(
+                              child: DPTextFormField(
+                                validator: controller.birthdayValidator,
+                                onChanged: (birthday) {
+                                  controller.birthday.value = birthday;
+                                },
+                                textInputAction: TextInputAction.next,
+                                label: '생년 월일',
+                                maxLength: 6,
+                                textInputType: TextInputType.number,
+                                hintText: '6자리로 입력',
+                              ),
                             ),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(height: 18),
-                      DPTextField(
-                        controller: controller.passwordFieldController,
-                        label: '비밀번호',
-                        isPassword: true,
-                        maxLength: 2,
-                        textInputType: TextInputType.number,
-                        hintText: '앞 2자리를 입력해주세요',
-                      ),
-                    ],
+                          ],
+                        ),
+                        const SizedBox(height: 18),
+                        DPTextFormField(
+                          validator: controller.passwordValidator,
+                          onChanged: (password) {
+                            controller.password.value = password;
+                          },
+                          textInputAction: TextInputAction.done,
+                          onEditingComplete: controller.createGeneralCard,
+                          label: '비밀번호',
+                          isPassword: true,
+                          maxLength: 2,
+                          textInputType: TextInputType.number,
+                          hintText: '앞 2자리를 입력해주세요',
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
             ),
-            DPKeyboardReactiveButton(
-              onTap: () {
-                controller.createGeneralCard();
-              },
-              padding: const EdgeInsets.all(24),
-              child: const Text('다음', style: TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.w600)),
-            ),
+            controller.obx(
+                (_) => Obx(() => DPKeyboardReactiveButton(
+                      disabled: !controller.buttonEnabled,
+                      onTap: controller.buttonEnabled ? controller.createGeneralCard : null,
+                      padding: const EdgeInsets.all(24),
+                      child: const Text('다음', style: TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.w600)),
+                    )),
+                onLoading: const DPKeyboardReactiveButton(
+                  disabled: true,
+                  padding: EdgeInsets.all(24),
+                  child: SizedBox(
+                    height: 20,
+                    width: 20,
+                    child: CircularProgressIndicator(color: Colors.white, strokeWidth: 2),
+                  ),
+                ))
           ],
         ),
       ),
