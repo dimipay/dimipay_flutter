35744f5a3d6d49d8f9a1f9028616919aed76da2b
uglyonlytoday <superjang3357@naver.com>
2022-08-13 00:04:57 +0900

    refactor : DPSnackBar

--- lib/app/widgets/snackbar.dart
+++ lib/app/widgets/snackbar.dart
@@ -4,14 +4,14 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class DPSnackBar {
-  Color _backgroundColor = DPColors.DARK5;
-  Color _textColor = DPColors.DARK1;
-
-  void open(String title, {String? message}) {
+  static void open(String title, {Color backgroundColor = DPColors.DARK5, Color textColor = DPColors.DARK1, HapticPattern? hapticFeedback}) {
+    if (hapticFeedback != null) {
+      HapticHelper.feedback(hapticFeedback);
+    }
     Get.rawSnackbar(
-      titleText: Text(title, style: TextStyle(fontFamily: 'Pretendard', fontWeight: FontWeight.w500, fontSize: 16, height: 1.2, color: _textColor)),
+      titleText: Text(title, style: TextStyle(fontFamily: 'Pretendard', fontWeight: FontWeight.w500, fontSize: 16, height: 1, color: textColor)),
       messageText: Container(),
-      backgroundColor: _backgroundColor,
+      backgroundColor: backgroundColor,
       borderRadius: 12,
       padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
       margin: const EdgeInsets.symmetric(horizontal: 24, vertical: 24),
@@ -20,15 +20,8 @@ class DPSnackBar {
   }
 }
 
-class DPErrorSnackBar extends DPSnackBar {
-  DPErrorSnackBar() {
-    _backgroundColor = DPColors.ERROR;
-    _textColor = Colors.white;
-  }
-
-  @override
+class DPErrorSnackBar {
   void open(String title, {String? message}) {
-    super.open(title, message: message);
-    HapticHelper.feedback(HapticPatterns.error);
+    DPSnackBar.open(title, backgroundColor: DPColors.ERROR, textColor: Colors.white, hapticFeedback: HapticPatterns.error);
   }
 }
