bbdf09be297c2eebc5fb3d4db20801a12ef870e8
uglyonlytoday <superjang3357@naver.com>
2022-08-13 22:48:12 +0900

    refactor : route

--- lib/app/routes/routes.dart
+++ lib/app/routes/routes.dart
@@ -1,27 +1,27 @@
 // ignore_for_file: constant_identifier_names
 
 abstract class Routes {
-  static const TEST = '/Test';
+  static const TEST = '/test';
   static const HOME = '/';
-  static const USER = '/Me';
-  static const MANAGEMETHOD = '/MainPage/ManageMethod';
-  static const REGISTERCARD = '/RegisterCard';
-  static const SELECTCHARGINGMETHOD = '/SelectChargingMethod';
-  static const EDITCARDNAME = '/EditCardName';
-  static const CARDFIN = '/CardFin';
-  static const CHARGINGFIN = '/ChargingFin';
-  static const CHARGINGAMOUNT = "/ChargingFin/ChargingAmount";
-  static const COUPON = "/Coupon";
-  static const HISTORY = '/History';
-  static const CHANGEPASSWORD = "/ChangePassword";
-  static const UNTITLED = "/Untitled";
-  static const TRANSACTION = "/Transaction";
-  static const EVENT = "/Event";
-  static const NOTIFICATION = "/Notification";
-  static const ISSUECOUPON = '/IssueCoupon';
-  static const PIN = "/Pin";
-  static const BIOMETRICAUTH = "/BiometricAuth";
-  static const PRINTERSETTING = "/PrinterSetting";
-  static const TRANSACTIONFIN = "/TransactionFin";
-  static const LOGIN = "/Login";
+  static const USER = '/me';
+  static const MANAGEMETHOD = '/paymentMethods';
+  static const REGISTERCARD = '/registerCard';
+  static const SELECTCHARGINGMETHOD = '/selectChargingMethod';
+  static const EDITCARDNAME = '/editCardName';
+  static const CARDFIN = '/cardFin';
+  static const CHARGINGFIN = '/chargingFin';
+  static const CHARGINGAMOUNT = "/chargingAmount";
+  static const COUPON = "/coupon";
+  static const HISTORY = '/history';
+  static const CHANGEPASSWORD = "/changePassword";
+  static const UNTITLED = "/untitled";
+  static const TRANSACTION = "/transaction";
+  static const EVENT = "/event";
+  static const NOTIFICATION = "/notification";
+  static const ISSUECOUPON = '/issueCoupon';
+  static const PIN = "/pin";
+  static const BIOMETRICAUTH = "/biometricAuth";
+  static const PRINTERSETTING = "/printerSetting";
+  static const TRANSACTIONFIN = "/transactionFin";
+  static const LOGIN = "/login";
 }
