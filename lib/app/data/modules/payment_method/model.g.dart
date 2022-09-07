ce152ae4e714ef5222573cbd9bc84d860d0f0ea1
uglyonlytoday <superjang3357@naver.com>
2022-09-07 20:46:43 +0900

    update model

--- lib/app/data/modules/payment_method/model.g.dart
+++ lib/app/data/modules/payment_method/model.g.dart
@@ -8,20 +8,18 @@ part of 'model.dart';
 
 PaymentMethod _$PaymentMethodFromJson(Map<String, dynamic> json) =>
     PaymentMethod(
-      systemId: json['systemId'] as String,
+      id: json['id'] as String,
       createdAt: DateTime.parse(json['createdAt'] as String),
       updatedAt: DateTime.parse(json['updatedAt'] as String),
       color: json['color'] as String?,
       name: json['name'] as String?,
-      ownerSid: json['ownerSid'] as String,
     );
 
 Map<String, dynamic> _$PaymentMethodToJson(PaymentMethod instance) =>
     <String, dynamic>{
-      'systemId': instance.systemId,
+      'id': instance.id,
       'createdAt': instance.createdAt.toIso8601String(),
       'updatedAt': instance.updatedAt.toIso8601String(),
       'color': instance.color,
       'name': instance.name,
-      'ownerSid': instance.ownerSid,
     };
