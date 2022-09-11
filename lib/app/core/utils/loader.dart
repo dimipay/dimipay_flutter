e7dd7aa86c187f6079d5c4b95be03edbbb61f0fa
Minyong Oh <dhalsdyd@gmail.com>
2022-09-11 22:28:54 +0900

    add local auth service

--- lib/app/core/utils/loader.dart
+++ lib/app/core/utils/loader.dart
@@ -2,6 +2,7 @@ import 'package:dimipay/app/data/provider/api.dart';
 import 'package:dimipay/app/data/services/auth/repository.dart';
 import 'package:dimipay/app/data/services/auth/service.dart';
 import 'package:dimipay/app/data/services/config/service.dart';
+import 'package:dimipay/app/data/services/local_auth/service.dart';
 import 'package:get/get.dart';
 import 'package:url_strategy/url_strategy.dart';
 
@@ -11,6 +12,7 @@ class AppLoader {
     await Future.wait([
       Get.putAsync<AuthService>(() => AuthService(AuthRepository(ApiProvider())).init()),
       Get.putAsync<AppConfigService>(() => AppConfigService().init()),
+      Get.putAsync<LocalAuthService>(() => LocalAuthService().init()),
     ]);
   }
 }
