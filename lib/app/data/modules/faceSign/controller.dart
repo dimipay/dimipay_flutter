95433f5550de37656fc1265c45545efa34c94d6b
Minyong Oh <dhalsdyd@gmail.com>
2022-09-16 22:02:38 +0900

    add snackbar FaceSignController

--- lib/app/data/modules/faceSign/controller.dart
+++ lib/app/data/modules/faceSign/controller.dart
@@ -1,32 +1,47 @@
 import 'package:dimipay/app/data/modules/coupon/model.dart';
 import 'package:dimipay/app/data/modules/faceSign/repository.dart';
 import 'package:dimipay/app/widgets/snackbar.dart';
+import 'package:dio/dio.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 
 class FaceSignController extends GetxController with StateMixin {
   final FaceSignRepository repository;
-  final Rx<bool> _gallaryOrCamera = false.obs;
+  final Rx<bool> _isCamera = true.obs;
 
   FaceSignController(this.repository);
 
-  registerFaceSign() async {
-    XFile? result;
+  Future<void> registerFaceSign() async {
+    XFile? image;
 
-    if (_gallaryOrCamera.value) {
-      result = await ImagePicker().pickImage(source: ImageSource.camera);
+    if (_isCamera.value) {
+      image = await ImagePicker().pickImage(source: ImageSource.camera);
     } else {
-      result = await ImagePicker().pickImage(source: ImageSource.gallery);
+      image = await ImagePicker().pickImage(source: ImageSource.gallery);
     }
 
-    if (result != null) {
-      await repository.register(result);
+    if (image != null) {
+      try {
+        final result = await repository.register(image);
+        if (result["code"] == "OK") {
+          DPSnackBar.open("얼굴 등록에 성공했어요.");
+        } else {
+          DPErrorSnackBar().open('얼굴 등록에 실패했습니다.');
+        }
+      } on DioError catch (e) {
+        rethrow;
+      }
     } else {
       DPSnackBar.open('No image selected');
     }
   }
 
-  deleteFaceSign() async {
-    await repository.delete();
+  Future<void> deleteFaceSign() async {
+    try {
+      await repository.delete();
+      DPSnackBar.open('얼굴 삭제가 완료되었습니다.');
+    } on DioError catch (e) {
+      rethrow;
+    }
   }
 }
