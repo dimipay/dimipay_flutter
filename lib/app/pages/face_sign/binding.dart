9530eb22de9e6f9b89468af2e52495131805e0c0
uglyonlytoday <superjang3357@naver.com>
2022-09-19 16:37:17 +0900

    fix : merge issue

--- lib/app/pages/face_sign/binding.dart
+++ lib/app/pages/face_sign/binding.dart
@@ -1,5 +1,5 @@
-import 'package:dimipay/app/data/modules/faceSign/repository.dart';
 import 'package:dimipay/app/data/modules/face_sign/controller.dart';
+import 'package:dimipay/app/data/modules/face_sign/repository.dart';
 import 'package:dimipay/app/data/provider/api.dart';
 import 'package:dimipay/app/pages/face_sign/controller.dart';
 import 'package:get/get.dart';
