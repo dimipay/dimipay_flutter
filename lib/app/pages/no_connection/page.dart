12f7d344882d1c964c653fbec87e2414b472cf08
Minyong Oh <dhalsdyd@gmail.com>
2022-09-21 22:30:36 +0900

    prevent to go back

--- lib/app/pages/no_connection/page.dart
+++ lib/app/pages/no_connection/page.dart
@@ -10,17 +10,20 @@ class NoConnectionPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Expanded(
-                  child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [SvgPicture.asset("asset/images/logo2.svg", width: 40), const Text('DIMIPAY', style: TextStyle(color: Color.fromRGBO(46, 164, 171, 1), fontFamily: 'Montserrat', fontSize: 22)), const SizedBox(height: 48), const DPPaymentCard(color: DPColors.MAIN_THEME, cardName: "WIFI ISSUE", cardNumber: "WIFI", isHorizontal: false, width: 153), const SizedBox(height: 32), const Text("인터넷 연결이 끊겼습니다.", style: TextStyle(color: DPColors.DARK1, fontFamily: 'NEXON Lv1 Gothic', fontSize: 20, fontWeight: FontWeight.w700))],
-              )),
-            ],
+      body: WillPopScope(
+        onWillPop: () async => false,
+        child: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                    child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [SvgPicture.asset("asset/images/logo2.svg", width: 40), const Text('DIMIPAY', style: TextStyle(color: Color.fromRGBO(46, 164, 171, 1), fontFamily: 'Montserrat', fontSize: 22)), const SizedBox(height: 48), const DPPaymentCard(color: DPColors.MAIN_THEME, cardName: "WIFI ISSUE", cardNumber: "WIFI", isHorizontal: false, width: 153), const SizedBox(height: 32), const Text("인터넷 연결이 끊겼습니다.", style: TextStyle(color: DPColors.DARK1, fontFamily: 'NEXON Lv1 Gothic', fontSize: 20, fontWeight: FontWeight.w700))],
+                )),
+              ],
+            ),
           ),
         ),
       ),
