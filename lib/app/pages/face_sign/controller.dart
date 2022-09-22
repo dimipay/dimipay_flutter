dc761c599c52a65e0ab7834c57161d98c3a01ede
uglyonlytoday <superjang3357@naver.com>
2022-09-22 18:33:02 +0900

    facesign 등록 여부에 따라 화면이 바뀜

--- lib/app/pages/face_sign/controller.dart
+++ lib/app/pages/face_sign/controller.dart
@@ -1,21 +1,45 @@
+import 'dart:developer';
+
+import 'package:dimipay/app/core/utils/haptic.dart';
 import 'package:dimipay/app/data/modules/face_sign/controller.dart';
+import 'package:dimipay/app/data/modules/user/controller.dart';
 import 'package:dimipay/app/widgets/snackbar.dart';
 import 'package:dio/dio.dart';
 import 'package:get/get.dart';
 
 class FaceSignPageController extends GetxController {
   FaceSignController faceSignController = Get.find<FaceSignController>();
-  Rx<bool> isRegistered = false.obs;
+  UserController userController = Get.find<UserController>();
+  Rx<bool> isFacesignRegistered = false.obs;
 
-  void registerFaceSign() {
-    faceSignController.registerFaceSign();
+  @override
+  void onInit() async {
+    super.onInit();
+    if (userController.user == null) {
+      await userController.fetchUser();
+    }
+    isFacesignRegistered.value = userController.user!.faceSignRegistered;
+  }
+
+  void registerFaceSign() async {
+    try {
+      await faceSignController.registerFaceSign();
+      isFacesignRegistered.value = true;
+      userController.user?.faceSignRegistered = true;
+
+      DPSnackBar.open("얼굴 등록에 성공했어요.", hapticFeedback: HapticPatterns.success);
+    } on DioError catch (e) {
+      DPErrorSnackBar().open(e.response!.data["message"]);
+      // ignore: empty_catches
+    } on Exception {}
   }
 
   void deleteFaceSign() async {
     try {
       await faceSignController.deleteFaceSign();
       DPSnackBar.open("등록된 얼굴을 삭제했어요.");
-      Get.back();
+      isFacesignRegistered.value = false;
+      userController.user?.faceSignRegistered = false;
     } on DioError catch (e) {
       DPErrorSnackBar().open(e.response!.data["message"]);
     }
