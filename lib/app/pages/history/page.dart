706082e81abb2dd8e496d6f50efae35c3d3a87f5
uglyonlytoday <superjang3357@naver.com>
2022-09-24 00:11:23 +0900

    feat : transaction history title

--- lib/app/pages/history/page.dart
+++ lib/app/pages/history/page.dart
@@ -1,15 +1,12 @@
 import 'package:dimipay/app/core/theme/color_theme.dart';
 import 'package:dimipay/app/core/theme/text_theme.dart';
-import 'package:dimipay/app/data/modules/transaction/controller.dart';
 import 'package:dimipay/app/data/modules/transaction/model.dart';
 import 'package:dimipay/app/pages/history/controller.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
 
 class HistoryPage extends GetView<HistoryPageController> {
-  HistoryPage({Key? key}) : super(key: key);
-  final TransactionController transactionController = Get.find<TransactionController>();
+  const HistoryPage({Key? key}) : super(key: key);
 
   Widget _buildTransactions(List<Transaction> transactions) {
     Map<DateTime, List<Transaction>> dailyTransactions = {};
@@ -29,12 +26,12 @@ class HistoryPage extends GetView<HistoryPageController> {
     return Column(children: children);
   }
 
-  Widget _couponArea() {
-    return transactionController.obx(
+  Widget _transactionArea() {
+    return controller.transactionController.obx(
       (state) {
         return _buildTransactions(state!);
       },
-      onLoading: const CircularProgressIndicator(color: DPColors.MAIN_THEME),
+      onLoading: const Center(child: CircularProgressIndicator(color: DPColors.MAIN_THEME)),
     );
   }
 
@@ -44,6 +41,7 @@ class HistoryPage extends GetView<HistoryPageController> {
       backgroundColor: Colors.white,
       appBar: AppBar(title: const Text('결제 기록')),
       body: SafeArea(
+        bottom: false,
         child: RefreshIndicator(
           color: DPColors.MAIN_THEME,
           onRefresh: controller.refreshData,
@@ -56,15 +54,14 @@ class HistoryPage extends GetView<HistoryPageController> {
                     padding: const EdgeInsets.symmetric(horizontal: 24),
                     child: Column(
                       children: [
-                        const SizedBox(height: 24),
-                        _couponArea(),
+                        const SizedBox(height: 24, width: double.infinity),
+                        _transactionArea(),
                         const SizedBox(height: 36),
                       ],
                     ),
                   ),
                 ),
               ),
-              // const TransactionCalendarViewer(),
             ],
           ),
         ),
@@ -82,7 +79,6 @@ class TransactionGroup extends StatelessWidget {
     List<Widget> childeren = [];
     transactions.sort((a, b) => a.createdAt.compareTo(b.createdAt));
     for (var transaction in transactions) {
-      childeren.add(const SizedBox(height: 18));
       childeren.add(TransactionWidget(transaction: transaction));
     }
     return Column(children: childeren);
@@ -101,16 +97,15 @@ class TransactionGroup extends StatelessWidget {
           children: [
             Text(
               '${date.month}월 ${date.day}일',
-              style: const TextStyle(fontFamily: 'Pretendard', fontSize: 16, height: 1.2, color: DPColors.DARK1),
+              style: DPTextTheme.REGULAR,
             ),
             Text(
               '$sum원',
-              style: const TextStyle(fontFamily: 'Pretendard', fontSize: 16, height: 1.2, color: DPColors.DARK1),
+              style: DPTextTheme.REGULAR_IMPORTANT,
             ),
           ],
         ),
-        const SizedBox(height: 12),
-        const Divider(color: DPColors.DARK6),
+        const SizedBox(height: 24),
         _buildTransactions(transactions),
         const SizedBox(height: 48),
       ],
@@ -122,122 +117,39 @@ class TransactionWidget extends StatelessWidget {
   final Transaction transaction;
   const TransactionWidget({Key? key, required this.transaction}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Flexible(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text(
-                "transaction.title",
-                style: DPTextTheme.REGULAR,
-              ),
-              const SizedBox(height: 4),
-              Text(
-                '${transaction.createdAt.hour}시 ${transaction.createdAt.minute}분',
-                style: DPTextTheme.DESCRIPTION,
-              ),
-            ],
-          ),
-        ),
-        const SizedBox(width: 53),
-        Text(
-          '${transaction.totalPrice}원',
-          style: const TextStyle(fontSize: 16, color: DPColors.MAIN_THEME, fontWeight: FontWeight.w600),
-        ),
-      ],
-    );
+  String getTransactionTitle() {
+    return transaction.products.map((e) => e.name).join(', ');
   }
-}
-
-class TransactionCalendarViewer extends StatelessWidget {
-  const TransactionCalendarViewer({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      decoration: const BoxDecoration(border: Border(top: BorderSide(color: Color(0xFFD8D8D8)))),
-      padding: const EdgeInsets.all(24),
-      child: Column(
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Row(
-            children: [
-              SvgPicture.asset('asset/images/arrow_left_6.svg'),
-              const SizedBox(width: 10),
-              const Text(
-                '2021년',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
-              ),
-              const SizedBox(width: 4),
-              const Text(
-                '4월',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
-              ),
-              const SizedBox(width: 10),
-              SvgPicture.asset('asset/images/arrow_right.svg'),
-            ],
-          ),
-          const SizedBox(height: 12),
-          ConstrainedBox(
-            constraints: const BoxConstraints(maxWidth: 400),
-            child: Row(
+          Flexible(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                TransactionCalendarViewerDateItem(date: DateTime(2021, 12, 19)),
-                const SizedBox(width: 8),
-                TransactionCalendarViewerDateItem(date: DateTime(2021, 12, 20), isHighlighted: true),
-                const SizedBox(width: 8),
-                TransactionCalendarViewerDateItem(date: DateTime(2021, 12, 21), isHighlighted: true),
-                const SizedBox(width: 8),
-                TransactionCalendarViewerDateItem(date: DateTime(2021, 12, 22)),
-                const SizedBox(width: 8),
-                TransactionCalendarViewerDateItem(date: DateTime(2021, 12, 23), isHighlighted: true),
-                const SizedBox(width: 8),
-                TransactionCalendarViewerDateItem(date: DateTime(2021, 12, 24)),
-                const SizedBox(width: 8),
-                TransactionCalendarViewerDateItem(date: DateTime(2021, 12, 25), isHighlighted: true),
+                Text(
+                  getTransactionTitle(),
+                  style: DPTextTheme.REGULAR,
+                ),
+                const SizedBox(height: 4),
+                Text(
+                  '${transaction.createdAt.hour}시 ${transaction.createdAt.minute}분',
+                  style: DPTextTheme.DESCRIPTION,
+                ),
               ],
             ),
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class TransactionCalendarViewerDateItem extends StatelessWidget {
-  final DateTime date;
-  final bool isHighlighted;
-  final weekdays = ['', '월', '화', '수', '목', '금', '토', '일'];
-  TransactionCalendarViewerDateItem({Key? key, required this.date, this.isHighlighted = false}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Flexible(
-      child: Column(
-        children: [
-          Text(weekdays[date.weekday], style: const TextStyle(color: Color.fromRGBO(0, 0, 0, 0.4))),
-          const SizedBox(height: 6),
-          AspectRatio(
-            aspectRatio: 1,
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                color: isHighlighted ? DPColors.MAIN_THEME : const Color(0xFFF9F9F9),
-              ),
-              child: Center(
-                child: Text(
-                  date.day.toString(),
-                  style: TextStyle(color: isHighlighted ? Colors.white : Colors.black),
-                ),
-              ),
-            ),
-          )
+          ),
+          const SizedBox(width: 53),
+          Text(
+            '${transaction.totalPrice}원',
+            style: const TextStyle(fontSize: 16, color: DPColors.MAIN_THEME, fontWeight: FontWeight.w600),
+          ),
         ],
       ),
     );
