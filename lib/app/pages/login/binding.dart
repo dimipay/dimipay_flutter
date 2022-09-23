140dbfa3cf1299fda8ae9abf1485f2d051f87a72
uglyonlytoday <superjang3357@naver.com>
2022-09-23 23:57:02 +0900

    replace connectivity binding

--- lib/app/pages/login/binding.dart
+++ lib/app/pages/login/binding.dart
@@ -1,3 +1,4 @@
+import 'package:dimipay/app/data/services/connectivity/service.dart';
 import 'package:dimipay/app/pages/login/controller.dart';
 import 'package:get/get.dart';
 
@@ -6,5 +7,6 @@ class LoginPageBinding implements Bindings {
   void dependencies() {
     Get.lazyPut<LoginPageController>(() => LoginPageController());
     Get.lazyPut<GyroScopeController>(() => GyroScopeController());
+    Get.put<ConnectivityService>(ConnectivityService());
   }
 }
