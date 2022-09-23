3ab6ddc63c8d607547e6e68d477aa76dbb03172c
uglyonlytoday <superjang3357@naver.com>
2022-09-23 12:32:40 +0900

    fix : merge error

--- lib/app/pages/manage_method/controller.dart
+++ lib/app/pages/manage_method/controller.dart
@@ -1,12 +1,11 @@
 import 'package:dimipay/app/core/utils/haptic.dart';
 import 'package:dimipay/app/data/modules/payment_method/controller.dart';
 import 'package:dimipay/app/data/modules/payment_method/model.dart';
-import 'package:dimipay/app/data/modules/payment_method/model.dart';
 import 'package:dimipay/app/widgets/snackbar.dart';
 import 'package:get/get.dart';
 
 class ManageMethodPageController extends GetxController {
-  Future<void> deletePaymentMethod(PaymentMethod paymentMethodPaymentMethod paymentMethod) async {
+  Future<void> deletePaymentMethod(PaymentMethod paymentMethod) async {
     PaymentMethodController paymentMethodController = Get.find<PaymentMethodController>();
     await paymentMethodController.deleteGeneralCard(paymentMethod);
     DPSnackBar.open('카드를 삭제했어요', hapticFeedback: HapticPatterns.success);
