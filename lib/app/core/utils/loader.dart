c1146dc15cf24caaef61f771fc9a6e1139c97b6f
uglyonlytoday <superjang3357@naver.com>
2022-09-23 23:57:02 +0900

    replace connectivity binding

--- lib/app/core/utils/loader.dart
+++ lib/app/core/utils/loader.dart
@@ -2,7 +2,6 @@ import 'package:dimipay/app/data/provider/api.dart';
 import 'package:dimipay/app/data/services/auth/repository.dart';
 import 'package:dimipay/app/data/services/auth/service.dart';
 import 'package:dimipay/app/data/services/config/service.dart';
-import 'package:dimipay/app/data/services/connectivity/service.dart';
 import 'package:dimipay/app/data/services/local_auth/service.dart';
 import 'package:get/get.dart';
 import 'package:url_strategy/url_strategy.dart';
@@ -10,7 +9,7 @@ import 'package:url_strategy/url_strategy.dart';
 class AppLoader {
   Future<void> load() async {
     setPathUrlStrategy();
-    Get.put<ConnectivityService>(ConnectivityService());
+
     await Future.wait([
       Get.putAsync<AuthService>(() => AuthService(AuthRepository(ApiProvider())).init()),
       Get.putAsync<AppConfigService>(() => AppConfigService().init()),
