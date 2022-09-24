b7ebd30543319c6e5381db574688191f75b683e2
uglyonlytoday <superjang3357@naver.com>
2022-09-24 23:33:57 +0900

    refactor eventController

--- lib/app/pages/event/controller.dart
+++ lib/app/pages/event/controller.dart
@@ -5,6 +5,14 @@ import 'package:get/get.dart';
 class EventPageController extends GetxController {
   EventController eventController = Get.find<EventController>();
 
+  @override
+  void onInit() {
+    if (eventController.events == null) {
+      eventController.fetchEvents();
+    }
+    super.onInit();
+  }
+
   Future<void> refreshEvents() async {
     HapticFeedback.mediumImpact();
     await eventController.fetchEvents();
