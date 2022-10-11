d503665fceeb5d5d8439e80d20cd080b7daf14c0
uglyonlytoday <superjang3357@naver.com>
2022-10-11 17:34:12 +0900

    diet : delete nexon lv1 gothic font

--- lib/app/pages/no_connection/page.dart
+++ lib/app/pages/no_connection/page.dart
@@ -1,4 +1,5 @@
 import 'package:dimipay/app/core/theme/color_theme.dart';
+import 'package:dimipay/app/core/theme/text_theme.dart';
 import 'package:dimipay/app/widgets/payment.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -19,7 +20,17 @@ class NoConnectionPage extends StatelessWidget {
                 Expanded(
                     child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
-                  children: [SvgPicture.asset("asset/images/logo2.svg", width: 40), const Text('DIMIPAY', style: TextStyle(color: Color.fromRGBO(46, 164, 171, 1), fontFamily: 'Montserrat', fontSize: 22)), const SizedBox(height: 48), const DPPaymentCard(color: DPColors.MAIN_THEME, cardName: "WIFI ISSUE", cardNumber: "WIFI", isHorizontal: false, width: 153), const SizedBox(height: 32), const Text("인터넷 연결이 끊겼습니다.", style: TextStyle(color: DPColors.DARK1, fontFamily: 'NEXON Lv1 Gothic', fontSize: 20, fontWeight: FontWeight.w700))],
+                  children: [
+                    SvgPicture.asset("asset/images/logo2.svg", width: 40),
+                    const Text('DIMIPAY', style: TextStyle(color: Color.fromRGBO(46, 164, 171, 1), fontFamily: 'Montserrat', fontSize: 22)),
+                    const SizedBox(height: 48),
+                    const DPPaymentCard(color: DPColors.MAIN_THEME, cardName: "WIFI ISSUE", cardNumber: "WIFI", isHorizontal: false, width: 153),
+                    const SizedBox(height: 32),
+                    const Text(
+                      "인터넷 연결이 끊겼습니다.",
+                      style: DPTextTheme.HEADER3,
+                    ),
+                  ],
                 )),
               ],
             ),
