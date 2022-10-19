98e24a6bac529616a1b5613b9ada79d694c91f83
uglyonlytoday <superjang3357@naver.com>
2022-10-19 18:43:43 +0900

    refactor upgrader

--- lib/app/data/services/upgrader/service.dart
+++ lib/app/data/services/upgrader/service.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
 import 'package:upgrader/upgrader.dart';
+import 'package:version/version.dart';
 
-class DPUpgraderMessages extends UpgraderMessages {
+class DPUpgradeMessages extends UpgraderMessages {
   @override
   String get title => '업데이트 설치 가능';
 
@@ -16,23 +17,39 @@ class DPUpgraderMessages extends UpgraderMessages {
   String get releaseNotes => '릴리즈 노트';
 }
 
+class DPForceUpgradeMessages extends DPUpgradeMessages {
+  @override
+  String get title => '업데이트 설치 안내';
+  @override
+  String get prompt => '중대한 버그 수정 및 보안 업데이트를 포함하므로 업데이트 해야합니다.';
+}
+
 class UpgraderService extends GetxService {
+  final Appcast appcast = Appcast();
+  late final Upgrader upgrader;
+  bool shouldForceUpgrade() {
+    return appcast.items!.where((appCastItem) => Version.parse(appCastItem.versionString!) > Version.parse(upgrader.currentInstalledVersion()!)).any((element) => element.isCriticalUpdate);
+  }
+
   //iOS Simulator 에서는 작동이 안되는 오류가 있는데, 이는 앱스토어가 깔려있지 않아 발생하는 문제로 실제 기기에서는 정상 작동합니다.
-  final upgrader = Upgrader(
-    appcastConfig: AppcastConfiguration(
-      supportedOS: ['android', 'ios'],
-    ),
-    shouldPopScope: () => true,
-    messages: DPUpgraderMessages(),
-    durationUntilAlertAgain: const Duration(days: 1),
-    debugLogging: !kReleaseMode, // REMOVE this for release builds
-    showIgnore: false,
-    dialogStyle: GetPlatform.isAndroid ? UpgradeDialogStyle.material : UpgradeDialogStyle.cupertino,
-  );
 
   Future<UpgraderService> init() async {
+    await appcast.parseAppcastItemsFromUri('https://terrible-termite-17.telebit.io/appcast.xml');
+
+    upgrader = Upgrader(
+      appcastConfig: AppcastConfiguration(
+        supportedOS: ['android', 'ios'],
+      ),
+      shouldPopScope: () => true,
+      debugDisplayAlways: true,
+      durationUntilAlertAgain: const Duration(days: 1),
+      debugLogging: !kReleaseMode, // REMOVE this for release builds
+      showIgnore: false,
+      dialogStyle: GetPlatform.isAndroid ? UpgradeDialogStyle.material : UpgradeDialogStyle.cupertino,
+    );
     await upgrader.initialize();
-    upgrader.showLater = !upgrader.belowMinAppVersion();
+    upgrader.showLater = !shouldForceUpgrade();
+    upgrader.messages = shouldForceUpgrade() ? DPForceUpgradeMessages() : DPUpgradeMessages();
     return this;
   }
 }
