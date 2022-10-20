eeec60b34342f4d836cc986942a4fc11d285ff20
uglyonlytoday <superjang3357@naver.com>
2022-10-20 21:08:19 +0900

    add FaceSignTip2Page

--- lib/app/pages/facesign_tip2/page.dart
+++ lib/app/pages/facesign_tip2/page.dart
@@ -1,13 +1,13 @@
 import 'package:dimipay/app/core/theme/color_theme.dart';
 import 'package:dimipay/app/core/theme/text_theme.dart';
-import 'package:dimipay/app/routes/routes.dart';
+import 'package:dimipay/app/pages/facesign_tip2/controller.dart';
 import 'package:dimipay/app/widgets/button.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:lottie/lottie.dart';
 
-class FaceSignTip1Page extends StatelessWidget {
-  const FaceSignTip1Page({super.key});
+class FaceSignTip2Page extends GetView<FaceSignTip2PageController> {
+  const FaceSignTip2Page({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -22,19 +22,22 @@ class FaceSignTip1Page extends StatelessWidget {
             children: [
               Expanded(
                 child: Center(
-                  child: LottieBuilder.asset('asset/lottie/mask_off.json', reverse: true),
+                  child: LottieBuilder.asset(
+                    'asset/lottie/tip_person.json',
+                    reverse: true,
+                  ),
                 ),
               ),
-              const Text('마스크를 잠시 벗어주세요', style: DPTextTheme.HEADER2),
+              const Text('정확한 인식을 위한 팁', style: DPTextTheme.HEADER2),
               const SizedBox(height: 16),
               const Text(
-                '정확한 인식을 위해 얼굴을 등록할 때에는 잠시 마스크를 벗어주세요. 포스에선 마스크를 착용하고 Face Sign을 사용할 수 있어요.',
+                '평온한 표정으로 카메라를 응시한 채 얼굴을 가운데에 맞추고 사진을 찍으세요. 평소에 자주 하는 머리 스타일과 메이크업을 한 상태로 진행해주세요.',
                 style: TextStyle(fontFamily: 'Pretendard', fontSize: 16, height: 1.4, color: DPColors.DARK3),
                 textAlign: TextAlign.center,
               ),
-              const SizedBox(height: 64),
+              const SizedBox(height: 72),
               DPMediumTextButton(
-                onTap: () => Get.toNamed(Routes.FACESIGN_TIP2),
+                onTap: controller.registerFaceSign,
                 text: "다음",
               ),
               const SizedBox(height: 16),
