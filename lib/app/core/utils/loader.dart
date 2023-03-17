b775a561b765ee566251ac8a72fa5572cb5c6765
doch13_ <dohui0922@dimigo.hs.kr>
2023-03-17 22:45:38 +0900

    fix: Screen Brightness - Platform Support Bug

--- lib/app/core/utils/loader.dart
+++ lib/app/core/utils/loader.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:dimipay/app/core/utils/token_reference.dart';
 import 'package:dimipay/app/data/provider/api.dart';
 import 'package:dimipay/app/data/services/auth/repository.dart';
@@ -17,8 +19,9 @@ class AppLoader {
     setPathUrlStrategy();
     KakaoSdk.init(nativeAppKey: TokenReference.KAKAO_NATIVE_KEY);
 
+    if (Platform.isIOS) { await ScreenBrightness().setAutoReset(false); }
+
     await Future.wait([
-      ScreenBrightness().setAutoReset(false),
       Firebase.initializeApp(options: kIsWeb ? TokenReference.FIREBASEOPTION : null),
       Get.putAsync<UpgraderService>(() => UpgraderService().init()),
       Get.putAsync<AuthService>(() => AuthService(AuthRepository(ApiProvider())).init()),
