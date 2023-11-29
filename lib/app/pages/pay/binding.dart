ff9dc0f74365adfcd0f0119b6c613f4cfca7c333
Inhwa Jang <superjang3357@naver.com>
2023-11-29 10:13:38 +0900

    refactor payService

--- lib/app/pages/pay/binding.dart
+++ lib/app/pages/pay/binding.dart
@@ -1,6 +1,5 @@
 import 'package:dimipay/app/data/modules/payment_method/controller.dart';
 import 'package:dimipay/app/data/modules/payment_method/repository.dart';
-import 'package:dimipay/app/data/services/pay/repository.dart';
 import 'package:dimipay/app/data/services/pay/service.dart';
 import 'package:dimipay/app/pages/pay/controller.dart';
 import 'package:get/get.dart';
@@ -10,6 +9,6 @@ class PayPageBinding implements Bindings {
   void dependencies() {
     Get.lazyPut<PayPageController>(() => PayPageController());
     Get.lazyPut<PaymentMethodController>(() => PaymentMethodController(PaymentMethodRepository()));
-    Get.lazyPut<PayService>(() => PayService(PayRepository()));
+    Get.lazyPut(() => PayService());
   }
 }
