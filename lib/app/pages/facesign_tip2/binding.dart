cecb337c02c2515218c8e762b4123a86f94146b7
Inhwa Jang <superjang3357@naver.com>
2023-11-29 10:19:35 +0900

    refactor FaceSignController

--- lib/app/pages/facesign_tip2/binding.dart
+++ lib/app/pages/facesign_tip2/binding.dart
@@ -1,5 +1,4 @@
 import 'package:dimipay/app/data/modules/face_sign/controller.dart';
-import 'package:dimipay/app/data/modules/face_sign/repository.dart';
 import 'package:dimipay/app/pages/facesign_tip2/controller.dart';
 import 'package:get/get.dart';
 
@@ -7,6 +6,6 @@ class FaceSignTip2PageBinding implements Bindings {
   @override
   void dependencies() {
     Get.lazyPut<FaceSignTip2PageController>(() => FaceSignTip2PageController());
-    Get.lazyPut<FaceSignController>(() => FaceSignController(FaceSignRepository()));
+    Get.lazyPut(() => FaceSignController());
   }
 }
