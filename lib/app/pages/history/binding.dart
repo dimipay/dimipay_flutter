041bef9add5113d59780a34e225dc195b32d1e61
Inhwa Jang <superjang3357@naver.com>
2023-11-29 10:15:47 +0900

    Refactor TransactionController

--- lib/app/pages/history/binding.dart
+++ lib/app/pages/history/binding.dart
@@ -1,12 +1,11 @@
 import 'package:dimipay/app/data/modules/transaction/controller.dart';
-import 'package:dimipay/app/data/modules/transaction/respository.dart';
 import 'package:dimipay/app/pages/history/controller.dart';
 import 'package:get/get.dart';
 
 class HistoryPageBinding implements Bindings {
   @override
   void dependencies() {
-    Get.put<TransactionController>(TransactionController(TransactionRepository()));
+    Get.put(TransactionController());
     Get.lazyPut<HistoryPageController>(() => HistoryPageController());
   }
 }
